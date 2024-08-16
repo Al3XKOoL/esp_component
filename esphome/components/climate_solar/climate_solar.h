f1f96e7d772cad4554de52dd642ab0cb89cab12d
Al3XKOoL <moonrotation@gmail.com>
2024-08-16 14:54:45 +0100

    Update climate_solar.h

--- esphome/components/climate_solar/climate_solar.h
+++ esphome/components/climate_solar/climate_solar.h
@@ -1,34 +1,38 @@
+#pragma once
+
 #include "esphome.h"
 
-namespace esphome {
-  namespace climate_solar {
+namespace climate_solar {
 
-    class ClimateSolar : public esphome::Component, public esphome::Climate {
-     public:
-      void setup() override;
-      void control(const esphome::climate::ClimateCall &call) override;
-      esphome::climate::ClimateTraits traits() override;
-      void loop() override;
+class ClimateSolar : public esphome::Component, public esphome::climate::Climate {
+ public:
+  // Métodos necesarios para la configuración del componente
+  void setup() override;
+  void loop() override;
 
-      // Variables configurables desde YAML
-      esphome::sensor::Sensor *temp_sun_;
-      esphome::sensor::Sensor *temp_watter_;
-      esphome::sensor::Sensor *temp_output_;
-      esphome::switch_::Switch *pump_switch_;
-      float temp_max_;
-      float diff_high_;
-      float diff_mid_;
-      float visual_min_temp_;
-      float visual_max_temp_;
-      float pump_power_;
+  // Métodos setters para las variables del YAML
+  void set_temp_sun(sensor::Sensor *temp_sun);
+  void set_temp_watter(sensor::Sensor *temp_watter);
+  void set_temp_output(sensor::Sensor *temp_output);
+  void set_temp_max(float temp_max);
+  void set_diff_high(float diff_high);
+  void set_diff_mid(float diff_mid);
+  void set_visual_min_temp(float visual_min_temp);
+  void set_visual_max_temp(float visual_max_temp);
+  void set_pump_power(float pump_power);
+  void set_pump_switch(esphome::switch_::Switch *pump_switch);
 
-     protected:
-      bool bomba_activa;
-      uint32_t cycle_start_time_;
-      std::vector<uint32_t> last_cycle_times_;
-      uint32_t daily_active_time_;
-      uint32_t last_reset_time_;
-    };
+ private:
+  sensor::Sensor *temp_sun_;
+  sensor::Sensor *temp_watter_;
+  sensor::Sensor *temp_output_;
+  esphome::switch_::Switch *pump_switch_;
+  float temp_max_;
+  float diff_high_;
+  float diff_mid_;
+  float visual_min_temp_;
+  float visual_max_temp_;
+  float pump_power_;
+};
 
-  }  // namespace climate_solar
-}  // namespace esphome
+}  // namespace climate_solar
