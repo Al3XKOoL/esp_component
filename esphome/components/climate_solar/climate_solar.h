b9a65b591743d169066c139dbb967cbbe3192c47
Al3XKOoL <moonrotation@gmail.com>
2024-08-16 15:11:23 +0100

    Update climate_solar.h

--- esphome/components/climate_solar/climate_solar.h
+++ esphome/components/climate_solar/climate_solar.h
@@ -1,44 +1,53 @@
-#ifndef CLIMATE_SOLAR_H
-#define CLIMATE_SOLAR_H
-
 #include "esphome.h"
 
-class ClimateSolar : public esphome::Component, public esphome::climate::Climate {
+namespace esphome {
+namespace climate_solar {
+
+class ClimateSolar : public climate::Climate, public Component {
  public:
-  void setup() override;
-  void control(const esphome::climate::ClimateCall &call) override;
-  esphome::climate::ClimateTraits traits() override;
-  void loop() override;
+  void set_temp_sun(sensor::Sensor *temp_sun) { temp_sun_ = temp_sun; }
+  void set_temp_watter(sensor::Sensor *temp_watter) { temp_watter_ = temp_watter; }
+  void set_temp_output(sensor::Sensor *temp_output) { temp_output_ = temp_output; }
+  void set_pump_switch(switch_::Switch *pump_switch) { pump_switch_ = pump_switch; }
 
-  // Métodos para configurar los parámetros desde la interfaz
-  void set_temp_max(float temp_max) { this->temp_max_ = temp_max; }
-  void set_diff_high(float diff_high) { this->diff_high_ = diff_high; }
-  void set_diff_mid(float diff_mid) { this->diff_mid_ = diff_mid; }
-  void set_visual_min_temp(float visual_min_temp) { this->visual_min_temp_ = visual_min_temp; }
-  void set_visual_max_temp(float visual_max_temp) { this->visual_max_temp_ = visual_max_temp; }
-  void set_pump_power(float pump_power) { this->pump_power_ = pump_power; }
+  void set_temp_max(float temp_max) { temp_max_ = temp_max; }
+  void set_diff_high(float diff_high) { diff_high_ = diff_high; }
+  void set_diff_mid(float diff_mid) { diff_mid_ = diff_mid; }
+  void set_visual_min_temp(float visual_min_temp) { visual_min_temp_ = visual_min_temp; }
+  void set_visual_max_temp(float visual_max_temp) { visual_max_temp_ = visual_max_temp; }
+  void set_pump_power(int pump_power) { pump_power_ = pump_power; }
 
-  // Métodos para configurar los parámetros desde el YAML
-  void set_parameters(esphome::sensor::Sensor *temp_sun, esphome::sensor::Sensor *temp_watter, esphome::sensor::Sensor *temp_output, float temp_max, float diff_high, float diff_mid, float visual_min_temp, float visual_max_temp, float pump_power);
+  void setup() override {
+    // Implementar la lógica de configuración inicial aquí
+  }
 
- private:
-  esphome::sensor::Sensor *temp_sun_;
-  esphome::sensor::Sensor *temp_watter_;
-  esphome::sensor::Sensor *temp_output_;
-  float temp_max_;
-  float diff_high_;
-  float diff_mid_;
-  float visual_min_temp_;
-  float visual_max_temp_;
-  float pump_power_;
-  bool bomba_activa = false;
-  uint32_t cycle_start_time_;
-  std::vector<uint32_t> last_cycle_times_;
-  uint32_t daily_active_time_;
-  uint32_t last_reset_time_;
-  esphome::sensor::Sensor *last_cycle_time_sensor_;
-  esphome::sensor::Sensor *daily_active_time_sensor_;
-  esphome::sensor::Sensor *daily_energy_consumption_sensor_;
+  void control(const climate::ClimateCall &call) override {
+    // Implementar control del clima aquí
+  }
+
+  climate::ClimateTraits traits() override {
+    auto traits = climate::ClimateTraits();
+    traits.set_supports_current_temperature(true);
+    traits.set_supports_two_point_target_temperature(false);
+    traits.set_visual_min_temperature(this->visual_min_temp_);
+    traits.set_visual_max_temperature(this->visual_max_temp_);
+    traits.set_supported_modes({climate::CLIMATE_MODE_OFF, climate::CLIMATE_MODE_HEAT});
+    return traits;
+  }
+
+ protected:
+  sensor::Sensor *temp_sun_{nullptr};
+  sensor::Sensor *temp_watter_{nullptr};
+  sensor::Sensor *temp_output_{nullptr};
+  switch_::Switch *pump_switch_{nullptr};
+
+  float temp_max_{37.5};
+  float diff_high_{1.0};
+  float diff_mid_{1.0};
+  float visual_min_temp_{25.0};
+  float visual_max_temp_{40.0};
+  int pump_power_{100};
 };
 
-#endif // CLIMATE_SOLAR_H
+}  // namespace climate_solar
+}  // namespace esphome
