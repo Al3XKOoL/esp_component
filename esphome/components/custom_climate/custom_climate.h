0e9e1aa3fa7e0d18f1211e7c5fced29193f8edc0
Al3XKOoL <moonrotation@gmail.com>
2024-08-16 20:15:38 +0100

    Update custom_climate.h

--- esphome/components/custom_climate/custom_climate.h
+++ esphome/components/custom_climate/custom_climate.h
@@ -23,22 +23,30 @@ class CustomClimate : public Component, public climate::Climate {
   void set_temperature_difference(float diff) { temperature_difference_ = diff; }
 
  protected:
+  // Sensores de temperatura
   sensor::Sensor *temperature_azotea_sensor_{nullptr};
   sensor::Sensor *temperature_spa_sensor_{nullptr};
   sensor::Sensor *temperature_caliente_sensor_{nullptr};
+
+  // Switch para controlar la bomba solar
   switch_::Switch *bomba_solar_switch_{nullptr};
 
-  float max_temperature_{37.5};
-  float temperature_difference_{2.0};
+  // Configuraciones adicionales
+  float max_temperature_{37.5};             // Temperatura máxima por defecto
+  float temperature_difference_{2.0};       // Diferencia de temperatura para controlar la bomba
 
-  unsigned long last_check_time_{0};
-  const unsigned long interval_seconds_{30};
+  unsigned long last_check_time_{0};        // Tiempo de la última verificación
+  const unsigned long interval_seconds_{30}; // Intervalo de verificación en segundos
 
   int64_t tiempo_inicio_{0};
   bool espera_{false};
   int64_t tiempo_espera_fin_{0};
 
+  // Método que se encarga de la lógica de control de la bomba
   void check_and_control_pump();
+
+  // Método para actualizar y publicar el estado del climatizador
+  void update_state();
 };
 
 }  // namespace custom_climate
