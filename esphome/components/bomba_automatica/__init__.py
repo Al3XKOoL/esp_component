f9e74a9afa01868df46aeca147d8917bfd4a4ab1
Al3XKOoL <moonrotation@gmail.com>
2024-08-16 17:35:34 +0100

    Update __init__.py

--- esphome/components/bomba_automatica/__init__.py
+++ esphome/components/bomba_automatica/__init__.py
@@ -1,9 +1,8 @@
 # external_components/bomba_automatica/__init__.py
 
-from esphome import config_validation as cv
-from esphome.components import component
-from esphome.components.component import Component
 import esphome.config_validation as cv
+from esphome.components import Component
+from esphome.components import switch
 
 # Definir el esquema de configuración
 BOMBA_AUTOMATICA_SCHEMA = cv.Schema({
@@ -14,12 +13,11 @@ BOMBA_AUTOMATICA_SCHEMA = cv.Schema({
 # Definir la función de configuración
 def setup_bomba_automatica(config):
     """Configura el componente bomba_automatica"""
-    from esphome.components import bomba_automatica as component
-    return component.BombaAutomatica(
-        bomba_id=config['id'],
-        espera_time=config.get('espera_time', 300)
-    )
+    from esphome.components.bomba_automatica import BombaAutomatica
+    component = BombaAutomatica()
+    component.set_bomba_id(config['id'])
+    component.set_espera_time(config.get('espera_time', 300))
+    return component
 
 # Registrar el componente en el esquema
 esphome.config_validation.register_component('bomba_automatica', BOMBA_AUTOMATICA_SCHEMA, setup_bomba_automatica)
-
