66b6e1a3cb94c9729097d8488c0ef223a8ede0e5
Al3XKOoL <moonrotation@gmail.com>
2024-08-16 16:46:29 +0100

    Update main.cpp

--- esphome/components/climate_solar/main.cpp
+++ esphome/components/climate_solar/main.cpp
@@ -1,10 +1,13 @@
 #include "esphome.h"
 #include "climate_solar.h"
 
+using namespace esphome;
+using namespace esphome::climate_solar;
+
 ClimateSolar *control_bomba;
 
 void setup() {
+  // Configuración inicial
   control_bomba = new ClimateSolar();
   App.register_component(control_bomba);
-  // Configurar sensores, switches, etc. si es necesario
 }
