bfe4d394d2729016f01c6158fc8bea05304a7b39
Al3XKOoL <moonrotation@gmail.com>
2024-08-16 17:34:49 +0100

    Update __init__.py

--- esphome/components/bomba_automatica/__init__.py
+++ esphome/components/bomba_automatica/__init__.py
@@ -1,32 +1,25 @@
 # external_components/bomba_automatica/__init__.py
 
-from esphome.components import Component
-from esphome.config_validation import cv, ConfigType
+from esphome import config_validation as cv
+from esphome.components import component
+from esphome.components.component import Component
 import esphome.config_validation as cv
-from esphome import core
 
-def validate_bomba_automatica_config(config):
-    """Validar la configuración del componente bomba_automatica"""
-    return config
-
+# Definir el esquema de configuración
 BOMBA_AUTOMATICA_SCHEMA = cv.Schema({
     cv.Required("id"): cv.string,
     cv.Optional("espera_time", default=300): cv.positive_int,
 })
 
-def setup_bomba_automatica(config, component):
+# Definir la función de configuración
+def setup_bomba_automatica(config):
     """Configura el componente bomba_automatica"""
-    bomba_id = config['id']
-    espera_time = config.get('espera_time', 300)
-    component.set_bomba_id(bomba_id)
-    component.set_espera_time(espera_time)
-
-def setup_component(config):
-    """Registra el componente bomba_automatica"""
-    from esphome.components import component
-    component = component.BombaAutomatica()
-    setup_bomba_automatica(config, component)
-    return component
+    from esphome.components import bomba_automatica as component
+    return component.BombaAutomatica(
+        bomba_id=config['id'],
+        espera_time=config.get('espera_time', 300)
+    )
 
 # Registrar el componente en el esquema
-esphome.config_validation.register_component('bomba_automatica', BOMBA_AUTOMATICA_SCHEMA, setup_component)
+esphome.config_validation.register_component('bomba_automatica', BOMBA_AUTOMATICA_SCHEMA, setup_bomba_automatica)
+
