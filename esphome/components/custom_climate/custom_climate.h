10f622e27c98abf62d39daa3499cf93b6230aa0d
Al3XKOoL <moonrotation@gmail.com>
2024-08-17 08:41:00 +0100

    Update custom_climate.h

--- esphome/components/custom_climate/custom_climate.h
+++ esphome/components/custom_climate/custom_climate.h
@@ -4,26 +4,22 @@
 #include "esphome/components/climate/climate.h"
 #include "esphome/components/time/real_time_clock.h"
 
-// Añade estas líneas para definir las macros de registro
-#define ESPHOME_LOG_LEVEL ESPHOME_LOG_LEVEL_DEBUG
-#include "esphome/core/log.h"
-
 namespace custom_climate {
 
 class CustomClimate : public esphome::climate::Climate, public esphome::Component {
  public:
-  void set_temp_sun(esphome::sensor::Sensor *temp_sun) { temp_sun_ = temp_sun; }
-  void set_temp_water(esphome::sensor::Sensor *temp_water) { temp_water_ = temp_water; }
-  void set_temp_output(esphome::sensor::Sensor *temp_output) { temp_output_ = temp_output; }
-  void set_temp_max(float temp_max) { temp_max_ = temp_max; }
-  void set_diff_high(float diff_high) { diff_high_ = diff_high; }
-  void set_diff_mid(float diff_mid) { diff_mid_ = diff_mid; }
-  void set_visual_min_temp(float visual_min_temp) { visual_min_temp_ = visual_min_temp; }
-  void set_visual_max_temp(float visual_max_temp) { visual_max_temp_ = visual_max_temp; }
-  void set_pump_power(float pump_power) { pump_power_ = pump_power; }
-  void set_pump_switch(esphome::switch_::Switch *pump_switch) { pump_switch_ = pump_switch; }
-  void set_sntp_time(esphome::time::RealTimeClock *sntp_time) { sntp_time_ = sntp_time; }
-  void set_homeassistant_time(esphome::time::RealTimeClock *homeassistant_time) { homeassistant_time_ = homeassistant_time; }
+  void set_sensor_temp_sol(esphome::sensor::Sensor *sensor_temp_sol) { sensor_temp_sol_ = sensor_temp_sol; }
+  void set_sensor_temp_agua(esphome::sensor::Sensor *sensor_temp_agua) { sensor_temp_agua_ = sensor_temp_agua; }
+  void set_sensor_temp_salida(esphome::sensor::Sensor *sensor_temp_salida) { sensor_temp_salida_ = sensor_temp_salida; }
+  void set_temperatura_maxima(float temperatura_maxima) { temperatura_maxima_ = temperatura_maxima; }
+  void set_diferencia_alta(float diferencia_alta) { diferencia_alta_ = diferencia_alta; }
+  void set_diferencia_media(float diferencia_media) { diferencia_media_ = diferencia_media; }
+  void set_temperatura_visual_minima(float temperatura_visual_minima) { temperatura_visual_minima_ = temperatura_visual_minima; }
+  void set_temperatura_visual_maxima(float temperatura_visual_maxima) { temperatura_visual_maxima_ = temperatura_visual_maxima; }
+  void set_potencia_bomba(float potencia_bomba) { potencia_bomba_ = potencia_bomba; }
+  void set_interruptor_bomba(esphome::switch_::Switch *interruptor_bomba) { interruptor_bomba_ = interruptor_bomba; }
+  void set_tiempo_sntp(esphome::time::RealTimeClock *tiempo_sntp) { tiempo_sntp_ = tiempo_sntp; }
+  void set_tiempo_homeassistant(esphome::time::RealTimeClock *tiempo_homeassistant) { tiempo_homeassistant_ = tiempo_homeassistant; }
 
   void setup() override;
   void loop() override;
@@ -31,26 +27,28 @@ class CustomClimate : public esphome::climate::Climate, public esphome::Componen
   void control(const esphome::climate::ClimateCall &call) override;
 
  protected:
-  esphome::sensor::Sensor *temp_sun_;
-  esphome::sensor::Sensor *temp_water_;
-  esphome::sensor::Sensor *temp_output_;
-  float temp_max_;
-  float diff_high_;
-  float diff_mid_;
-  float visual_min_temp_;
-  float visual_max_temp_;
-  float pump_power_;
-  esphome::switch_::Switch *pump_switch_;
-  esphome::time::RealTimeClock *sntp_time_{nullptr};
-  esphome::time::RealTimeClock *homeassistant_time_{nullptr};
+  esphome::sensor::Sensor *sensor_temp_sol_;
+  esphome::sensor::Sensor *sensor_temp_agua_;
+  esphome::sensor::Sensor *sensor_temp_salida_;
+  float temperatura_maxima_;
+  float diferencia_alta_;
+  float diferencia_media_;
+  float temperatura_visual_minima_;
+  float temperatura_visual_maxima_;
+  float potencia_bomba_;
+  esphome::switch_::Switch *interruptor_bomba_;
+  esphome::time::RealTimeClock *tiempo_sntp_{nullptr};
+  esphome::time::RealTimeClock *tiempo_homeassistant_{nullptr};
 
-  unsigned long last_check_time_{0};
-  const unsigned long interval_seconds_{30};
+  unsigned long ultimo_tiempo_verificacion_{0};
+  const unsigned long intervalo_segundos_{30};
   bool espera_{false};
   int64_t tiempo_espera_fin_{0};
   int64_t tiempo_inicio_{0};
   int conteo_encendidos_{0};
   int64_t tiempo_encendida_{0};
+
+  void log_mensaje(const char* nivel, const char* formato, ...);
 };
 
 }  // namespace custom_climate
