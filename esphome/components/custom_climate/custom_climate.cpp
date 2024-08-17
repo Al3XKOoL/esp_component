113a5d8301fcac6b9e94c0420b497cf9196ddd13
Al3XKOoL <moonrotation@gmail.com>
2024-08-17 14:39:59 +0100

    add_

--- esphome/components/custom_climate/custom_climate.cpp
+++ esphome/components/custom_climate/custom_climate.cpp
@@ -30,7 +30,7 @@ void CustomClimate::setup() {
 
   // Restaurar el estado guardado si se habilitó la opción
   if (restore_state_) {
-    auto restore = this->restore_state;
+    auto restore = this->restore_state_;
     if (restore.mode != CLIMATE_MODE_OFF) {
       this->mode = restore.mode;
     }
