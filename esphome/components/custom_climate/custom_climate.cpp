d7c816b8f13d4d1057cc89c5dcf998f346e1a0f9
Al3XKOoL <moonrotation@gmail.com>
2024-08-17 08:30:33 +0100

    Update custom_climate.cpp

--- esphome/components/custom_climate/custom_climate.cpp
+++ esphome/components/custom_climate/custom_climate.cpp
@@ -16,7 +16,7 @@ void CustomClimate::loop() {
   if (current_time - last_check_time_ >= interval_seconds_ * 1000) {
     last_check_time_ = current_time;
 
-    ESP_LOGI(TAG, "Ejecutando loop()");
+    esph_log_i(TAG, "Ejecutando loop()");
 
     // Obtener tiempo actual
     int64_t tiempo_actual = 0;
@@ -30,9 +30,9 @@ void CustomClimate::loop() {
     if (espera_) {
       if (tiempo_actual >= tiempo_espera_fin_) {
         espera_ = false;
-        ESP_LOGI(TAG, "Reanudando verificaciones después de espera de 5 minutos.");
+        esph_log_i(TAG, "Reanudando verificaciones después de espera de 5 minutos.");
       } else {
-        ESP_LOGI(TAG, "En espera hasta %lld", tiempo_espera_fin_);
+        esph_log_i(TAG, "En espera hasta %lld", tiempo_espera_fin_);
         return;
       }
     }
@@ -44,22 +44,22 @@ void CustomClimate::loop() {
         pump_switch_->turn_on();
         conteo_encendidos_++;
         tiempo_inicio_ = tiempo_actual;
-        ESP_LOGI(TAG, "Bomba encendida debido a la temperatura adecuada");
+        esph_log_i(TAG, "Bomba encendida debido a la temperatura adecuada");
       } else {
-        ESP_LOGI(TAG, "Bomba ya está encendida");
+        esph_log_i(TAG, "Bomba ya está encendida");
       }
     } else {
       if (estado_bomba_actual) {
         pump_switch_->turn_off();
         int64_t tiempo_total_encendido = tiempo_actual - tiempo_inicio_;
         tiempo_encendida_ += tiempo_total_encendido;
-        ESP_LOGI(TAG, "Bomba apagada debido a temperatura inadecuada");
-        ESP_LOGW(TAG, "Tiempo total de funcionamiento de la bomba: %lld segundos", tiempo_total_encendido);
+        esph_log_i(TAG, "Bomba apagada debido a temperatura inadecuada");
+        esph_log_w(TAG, "Tiempo total de funcionamiento de la bomba: %lld segundos", tiempo_total_encendido);
         espera_ = true;
         tiempo_espera_fin_ = tiempo_actual + 300; // 5 minutos en segundos
         return;
       } else {
-        ESP_LOGI(TAG, "Bomba ya está apagada");
+        esph_log_i(TAG, "Bomba ya está apagada");
         return;
       }
     }
@@ -68,8 +68,8 @@ void CustomClimate::loop() {
       pump_switch_->turn_off();
       int64_t tiempo_total_encendido = tiempo_actual - tiempo_inicio_;
       tiempo_encendida_ += tiempo_total_encendido;
-      ESP_LOGI(TAG, "Bomba apagada debido a temperatura de salida insuficiente");
-      ESP_LOGW(TAG, "Tiempo total de funcionamiento de la bomba: %lld segundos", tiempo_total_encendido);
+      esph_log_i(TAG, "Bomba apagada debido a temperatura de salida insuficiente");
+      esph_log_w(TAG, "Tiempo total de funcionamiento de la bomba: %lld segundos", tiempo_total_encendido);
       espera_ = true;
       tiempo_espera_fin_ = tiempo_actual + 300; // 5 minutos en segundos
       return;
@@ -77,16 +77,16 @@ void CustomClimate::loop() {
 
     if (estado_bomba_actual) {
       int64_t tiempo_transcurrido = tiempo_actual - tiempo_inicio_;
-      ESP_LOGW(TAG, "Tiempo transcurrido de funcionamiento de la bomba: %02d:%02d:%02d",
+      esph_log_w(TAG, "Tiempo transcurrido de funcionamiento de la bomba: %02d:%02d:%02d",
                (int)(tiempo_transcurrido / 3600),
                (int)((tiempo_transcurrido % 3600) / 60),
                (int)(tiempo_transcurrido % 60));
     }
 
-    ESP_LOGW(TAG, "Diferencia Sol-Agua: %.2f°C", temp_sun_->state - temp_water_->state);
-    ESP_LOGW(TAG, "Diferencia Salida-Agua: %.2f°C", temp_output_->state - temp_water_->state);
-    ESP_LOGW(TAG, "Estado de la bomba: %d", pump_switch_->state);
-    ESP_LOGI(TAG, "Conteo de encendidos de la bomba: %d", conteo_encendidos_);
+    esph_log_w(TAG, "Diferencia Sol-Agua: %.2f°C", temp_sun_->state - temp_water_->state);
+    esph_log_w(TAG, "Diferencia Salida-Agua: %.2f°C", temp_output_->state - temp_water_->state);
+    esph_log_w(TAG, "Estado de la bomba: %d", pump_switch_->state);
+    esph_log_i(TAG, "Conteo de encendidos de la bomba: %d", conteo_encendidos_);
 
     this->publish_state();
   }
