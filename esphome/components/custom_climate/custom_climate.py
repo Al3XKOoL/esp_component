9b48cabbe0108716b6f54da59794190a0352cb73
Al3XKOoL <moonrotation@gmail.com>
2024-08-17 08:49:42 +0100

    Update and rename climate.py to custom_climate.py

---
+++ esphome/components/custom_climate/custom_climate.py
@@ -0,0 +1,66 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import climate, sensor, switch, time
+from esphome.const import CONF_ID
+
+CONF_SENSOR_TEMP_SOL = "sensor_temp_sol"
+CONF_SENSOR_TEMP_AGUA = "sensor_temp_agua"
+CONF_SENSOR_TEMP_SALIDA = "sensor_temp_salida"
+CONF_TEMPERATURA_MAXIMA = "temperatura_maxima"
+CONF_DIFERENCIA_ALTA = "diferencia_alta"
+CONF_DIFERENCIA_MEDIA = "diferencia_media"
+CONF_TEMPERATURA_VISUAL_MINIMA = "temperatura_visual_minima"
+CONF_TEMPERATURA_VISUAL_MAXIMA = "temperatura_visual_maxima"
+CONF_POTENCIA_BOMBA = "potencia_bomba"
+CONF_INTERRUPTOR_BOMBA = "interruptor_bomba"
+CONF_TIEMPO_SNTP = "tiempo_sntp"
+CONF_TIEMPO_HOMEASSISTANT = "tiempo_homeassistant"
+
+custom_climate_ns = cg.esphome_ns.namespace('custom_climate')
+CustomClimate = custom_climate_ns.class_('CustomClimate', climate.Climate, cg.Component)
+
+CONFIG_SCHEMA = climate.CLIMATE_SCHEMA.extend({
+    cv.GenerateID(): cv.declare_id(CustomClimate),
+    cv.Required(CONF_SENSOR_TEMP_SOL): cv.use_id(sensor.Sensor),
+    cv.Required(CONF_SENSOR_TEMP_AGUA): cv.use_id(sensor.Sensor),
+    cv.Required(CONF_SENSOR_TEMP_SALIDA): cv.use_id(sensor.Sensor),
+    cv.Required(CONF_TEMPERATURA_MAXIMA): cv.float_,
+    cv.Required(CONF_DIFERENCIA_ALTA): cv.float_,
+    cv.Required(CONF_DIFERENCIA_MEDIA): cv.float_,
+    cv.Required(CONF_TEMPERATURA_VISUAL_MINIMA): cv.float_,
+    cv.Required(CONF_TEMPERATURA_VISUAL_MAXIMA): cv.float_,
+    cv.Required(CONF_POTENCIA_BOMBA): cv.float_,
+    cv.Required(CONF_INTERRUPTOR_BOMBA): cv.use_id(switch.Switch),
+    cv.Optional(CONF_TIEMPO_SNTP): cv.use_id(time.RealTimeClock),
+    cv.Optional(CONF_TIEMPO_HOMEASSISTANT): cv.use_id(time.RealTimeClock),
+}).extend(cv.COMPONENT_SCHEMA)
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    await climate.register_climate(var, config)
+    
+    cg.add_define("USE_CUSTOM_CLIMATE")
+    
+    sensor_temp_sol = await cg.get_variable(config[CONF_SENSOR_TEMP_SOL])
+    cg.add(var.set_sensor_temp_sol(sensor_temp_sol))
+    sensor_temp_agua = await cg.get_variable(config[CONF_SENSOR_TEMP_AGUA])
+    cg.add(var.set_sensor_temp_agua(sensor_temp_agua))
+    sensor_temp_salida = await cg.get_variable(config[CONF_SENSOR_TEMP_SALIDA])
+    cg.add(var.set_sensor_temp_salida(sensor_temp_salida))
+    cg.add(var.set_temperatura_maxima(config[CONF_TEMPERATURA_MAXIMA]))
+    cg.add(var.set_diferencia_alta(config[CONF_DIFERENCIA_ALTA]))
+    cg.add(var.set_diferencia_media(config[CONF_DIFERENCIA_MEDIA]))
+    cg.add(var.set_temperatura_visual_minima(config[CONF_TEMPERATURA_VISUAL_MINIMA]))
+    cg.add(var.set_temperatura_visual_maxima(config[CONF_TEMPERATURA_VISUAL_MAXIMA]))
+    cg.add(var.set_potencia_bomba(config[CONF_POTENCIA_BOMBA]))
+    interruptor_bomba = await cg.get_variable(config[CONF_INTERRUPTOR_BOMBA])
+    cg.add(var.set_interruptor_bomba(interruptor_bomba))
+
+    if CONF_TIEMPO_SNTP in config:
+        tiempo_sntp = await cg.get_variable(config[CONF_TIEMPO_SNTP])
+        cg.add(var.set_tiempo_sntp(tiempo_sntp))
+
+    if CONF_TIEMPO_HOMEASSISTANT in config:
+        tiempo_homeassistant = await cg.get_variable(config[CONF_TIEMPO_HOMEASSISTANT])
+        cg.add(var.set_tiempo_homeassistant(tiempo_homeassistant))
