166b3599d7c8b92c8a0cfc1bc4ace2a8de959f6c
Al3XKOoL <moonrotation@gmail.com>
2024-08-18 14:48:48 +0100

    Claude

--- esphome/components/custom_climate/custom_climate.cpp
+++ esphome/components/custom_climate/custom_climate.cpp
@@ -11,10 +11,9 @@ static const char *TAG = "custom_climate";
 
 void CustomClimate::setup() {
   this->mode = climate::CLIMATE_MODE_OFF;
-  this->current_temperature = get_current_temperature();  // Inicializar la temperatura actual
-  this->target_temperature = 37.0;  // Temperatura objetivo predeterminada
+  this->current_temperature = get_current_temperature();
+  this->target_temperature = 37.0;
 
-  // Inicializar valores de los números
   if (this->diferencia_media_number_ != nullptr) {
     this->diferencia_media_ = this->diferencia_media_number_->state;
   }
@@ -22,9 +21,12 @@ void CustomClimate::setup() {
     this->diferencia_alta_ = this->diferencia_alta_number_->state;
   }
 
-  // Inicializar otros componentes y variables
   this->ultimo_tiempo_verificacion_ = millis();
   this->ultimo_reset_diario_ = this->obtener_tiempo_actual();
+
+  if (this->interruptor_bomba_ != nullptr && this->interruptor_bomba_->state) {
+    this->estado_actual_ = COMPROBACION_CONTINUA;
+  }
 
   this->publish_state();
   this->log_mensaje("INFO", "Setup completado");
@@ -32,10 +34,10 @@ void CustomClimate::setup() {
 
 void CustomClimate::loop() {
   unsigned long tiempo_actual = millis();
-  if (tiempo_actual - this->ultimo_tiempo_verificacion_ >= this->intervalo_segundos_ * 1000) {
+
+  if (tiempo_actual - this->ultimo_tiempo_verificacion_ >= 2000) {
     this->ultimo_tiempo_verificacion_ = tiempo_actual;
 
-    // Actualizar la temperatura actual
     float temp_actual = get_current_temperature();
     if (!std::isnan(temp_actual)) {
       this->current_temperature = temp_actual;
@@ -43,19 +45,15 @@ void CustomClimate::loop() {
       this->log_mensaje("ERROR", "La temperatura actual es NaN, revisa el sensor de temperatura de agua.");
     }
 
-    // Continuar con el control de temperatura
     if (this->mode == climate::CLIMATE_MODE_HEAT) {
-      this->log_mensaje("DEBUG", "Modo HEAT activo, iniciando control de bomba");
       this->control_bomba();
     } else {
-      this->log_mensaje("DEBUG", "Modo no es HEAT, apagando bomba");
       this->apagar_bomba();
     }
 
     this->actualizar_consumo();
     this->reset_consumo_diario();
 
-    // Publicar estados de los sensores
     if (this->conteo_encendidos_sensor_ != nullptr) {
       this->conteo_encendidos_sensor_->publish_state(this->conteo_encendidos_);
     }
@@ -71,39 +69,140 @@ void CustomClimate::loop() {
 
     this->publish_state();
   }
+}
 
-  // Verificar si la estabilización ha terminado
-  if (this->estabilizando_ && (millis() - this->tiempo_estabilizacion_inicio_ >= 10000)) {
-    this->estabilizando_ = false;
-    this->log_mensaje("DEBUG", "Estabilización completada");
+void CustomClimate::control_bomba() {
+  switch (this->estado_actual_) {
+    case COMPROBACION_INICIAL:
+      this->comprobacion_inicial();
+      break;
+    case COMPROBACION_CONTINUA:
+      this->comprobacion_continua();
+      break;
+    case MODO_INTERMITENTE:
+      this->modo_intermitente();
+      break;
+    case ESPERA_APAGADO:
+      this->espera_apagado();
+      break;
+    case VERIFICACION_TARGET:
+      this->verificacion_target();
+      break;
+  }
+}
+
+void CustomClimate::comprobacion_inicial() {
+  unsigned long tiempo_actual = millis();
+  if (tiempo_actual - this->ultimo_tiempo_verificacion_inicial_ >= this->intervalo_verificacion_inicial_) {
+    this->ultimo_tiempo_verificacion_inicial_ = tiempo_actual;
+
+    float temp_sol = this->sensor_temp_sol_->state;
+    float temp_agua = this->get_current_temperature();
+
+    if (temp_sol >= (temp_agua + this->diferencia_alta_) && temp_agua < this->target_temperature) {
+      this->encender_bomba();
+      this->estado_actual_ = COMPROBACION_CONTINUA;
+    }
+  }
+}
+
+void CustomClimate::comprobacion_continua() {
+  unsigned long tiempo_actual = millis();
+  if (tiempo_actual - this->ultimo_tiempo_verificacion_continua_ >= this->intervalo_verificacion_continua_) {
+    this->ultimo_tiempo_verificacion_continua_ = tiempo_actual;
+
+    float temp_caliente = this->sensor_temp_salida_->state;
+    float temp_agua = this->get_current_temperature();
+
+    if (temp_caliente >= (temp_agua + this->diferencia_media_) && temp_agua < this->target_temperature) {
+      if (temp_agua >= (this->target_temperature - this->temperatura_cerca_)) {
+        this->estado_actual_ = MODO_INTERMITENTE;
+      }
+    } else {
+      this->apagar_bomba();
+      this->tiempo_espera_ = tiempo_actual;
+      this->estado_actual_ = ESPERA_APAGADO;
+    }
+  }
+}
+
+void CustomClimate::modo_intermitente() {
+  unsigned long tiempo_actual = millis();
+  if (tiempo_actual - this->ultimo_tiempo_verificacion_continua_ >= this->intervalo_verificacion_continua_) {
+    this->ultimo_tiempo_verificacion_continua_ = tiempo_actual;
+
+    float temp_caliente = this->sensor_temp_salida_->state;
+    float temp_agua = this->get_current_temperature();
+
+    if (temp_caliente >= (temp_agua + this->diferencia_media_)) {
+      if (!this->interruptor_bomba_->state) {
+        this->encender_bomba();
+      }
+    } else {
+      this->apagar_bomba();
+    }
+
+    if (temp_agua >= this->target_temperature) {
+      this->apagar_bomba();
+      this->estado_actual_ = VERIFICACION_TARGET;
+    }
+  }
+}
+
+void CustomClimate::espera_apagado() {
+  unsigned long tiempo_actual = millis();
+  if (tiempo_actual - this->tiempo_espera_ >= this->tiempo_espera_apagado_) {
+    this->estado_actual_ = COMPROBACION_INICIAL;
+  }
+}
+
+void CustomClimate::verificacion_target() {
+  unsigned long tiempo_actual = millis();
+  if (tiempo_actual - this->ultimo_tiempo_verificacion_continua_ >= this->intervalo_verificacion_target_) {
+    this->ultimo_tiempo_verificacion_continua_ = tiempo_actual;
+
+    float temp_agua = this->get_current_temperature();
+    if (temp_agua < this->target_temperature) {
+      this->estado_actual_ = COMPROBACION_INICIAL;
+    }
+  }
+}
+
+void CustomClimate::encender_bomba() {
+  this->log_mensaje("WARN", "Encendiendo bomba");
+  if (this->interruptor_bomba_ != nullptr) {
+    this->interruptor_bomba_->turn_on();
+    this->conteo_encendidos_++;
+    this->tiempo_inicio_ = this->obtener_tiempo_actual();
+    this->action = climate::CLIMATE_ACTION_HEATING;
+    this->publish_state();
+  } else {
+    this->log_mensaje("ERROR", "Interruptor de bomba no configurado");
+  }
+}
+
+void CustomClimate::apagar_bomba() {
+  this->log_mensaje("WARN", "Apagando bomba");
+  if (this->interruptor_bomba_ != nullptr) {
+    this->interruptor_bomba_->turn_off();
+    this->actualizar_consumo();
+    this->action = climate::CLIMATE_ACTION_IDLE;
+    this->publish_state();
+  } else {
+    this->log_mensaje("ERROR", "Interruptor de bomba no configurado");
   }
 }
 
 void CustomClimate::control(const climate::ClimateCall &call) {
   if (call.get_mode().has_value()) {
-    climate::ClimateMode mode = call.get_mode().value();
-    switch (mode) {
-      case climate::CLIMATE_MODE_HEAT:
-        this->mode = climate::CLIMATE_MODE_HEAT;
-        break;
-      case climate::CLIMATE_MODE_OFF:
-        this->mode = climate::CLIMATE_MODE_OFF;
-        this->apagar_bomba();
-        break;
-      default:
-        break;
-    }
-  }
-
-  if (call.get_target_temperature().has_value()) {
-    this->target_temperature = call.get_target_temperature().value();
+    this->mode = *call.get_mode();
     this->publish_state();
-  } else if (this->mode == climate::CLIMATE_MODE_HEAT) {
-    // Actualizar la temperatura objetivo incluso si no ha cambiado
+  }
+  if (call.get_target_temperature().has_value()) {
+    this->target_temperature = *call.get_target_temperature();
     this->publish_state();
   }
 }
-
 
 climate::ClimateTraits CustomClimate::traits() {
   auto traits = climate::ClimateTraits();
@@ -115,152 +214,6 @@ climate::ClimateTraits CustomClimate::traits() {
   traits.add_supported_mode(climate::CLIMATE_MODE_OFF);
   traits.add_supported_mode(climate::CLIMATE_MODE_HEAT);
   return traits;
-}
-
-bool CustomClimate::control_bomba() {
-  this->log_mensaje("DEBUG", "Iniciando control de bomba");
-
-  if (this->espera_) {
-    int64_t tiempo_actual = this->obtener_tiempo_actual();
-    if (tiempo_actual >= this->tiempo_espera_fin_) {
-      this->log_mensaje("DEBUG", "Fin del tiempo de espera");
-      this->espera_ = false;
-    } else {
-      int64_t tiempo_restante = this->tiempo_espera_fin_ - tiempo_actual;
-      String tiempo_espera_formateado = this->formatear_tiempo_espera(tiempo_restante);
-      time_t tiempo_actual_time_t = tiempo_actual;
-      struct tm *tiempo_actual_tm = localtime(&tiempo_actual_time_t);
-      char buffer_fecha[20];
-      strftime(buffer_fecha, sizeof(buffer_fecha), "%Y-%m-%d %H:%M:%S", tiempo_actual_tm);
-      this->log_mensaje("DEBUG", "En espera hasta %s (actual: %s)", tiempo_espera_formateado.c_str(), buffer_fecha);
-      return true;
-    }
-  }
-
-  bool cerca_objetivo = this->modo_cerca_temperatura_objetivo();
-  this->log_mensaje("DEBUG", "Cerca del objetivo: %s", cerca_objetivo ? "Sí" : "No");
-
-  if (cerca_objetivo) {
-    this->control_bomba_cerca_objetivo();
-  } else {
-    this->control_bomba_normal();
-  }
-
-  this->log_mensaje("DEBUG", "Control de bomba completado");
-  return true;
-}
-
-bool CustomClimate::modo_cerca_temperatura_objetivo() {
-  return (this->current_temperature >= (this->target_temperature - this->temperatura_cerca_));
-}
-
-void CustomClimate::control_bomba_cerca_objetivo() {
-  this->log_mensaje("DEBUG", "Control de bomba cerca del objetivo");
-
-  if (this->espera_) {
-    // Detener la bomba durante el tiempo de espera proporcional
-    if (this->interruptor_bomba_->state) {
-      this->apagar_bomba();
-    }
-    this->action = climate::CLIMATE_ACTION_IDLE;
-    this->publish_state();
-    return;
-  }
-
-  if (this->target_temperature != this->previous_target_temperature) {
-    // La temperatura objetivo ha cambiado, reiniciar el control de la bomba
-    this->previous_target_temperature = this->target_temperature;
-    this->apagar_bomba();
-    this->espera_ = false;
-  }
-
-  if (this->interruptor_bomba_->state && this->temperatura_alcanzada()) {
-    this->apagar_bomba();
-    this->action = climate::CLIMATE_ACTION_OFF;
-    this->publish_state();
-  } else if (!this->interruptor_bomba_->state && this->diferencia_temperatura_suficiente()) {
-    this->encender_bomba();
-    this->esperar_estabilizacion();
-    this->activar_espera_proporcional();
-    this->action = climate::CLIMATE_ACTION_IDLE;
-    this->publish_state();
-  } else if (this->interruptor_bomba_->state && !this->temperatura_alcanzada()) {
-    this->action = climate::CLIMATE_ACTION_HEATING;
-    this->publish_state();
-  }
-}
-
-void CustomClimate::control_bomba_normal() {
-  this->log_mensaje("DEBUG", "Control de bomba normal");
-  if (!this->interruptor_bomba_->state && this->diferencia_temperatura_suficiente()) {
-    this->encender_bomba();
-    this->esperar_estabilizacion();
-    this->action = climate::CLIMATE_ACTION_HEATING; // Actualizar la acción a "HEATING"
-    this->publish_state();
-  } else if (this->interruptor_bomba_->state && !this->diferencia_temperatura_suficiente()) {
-    this->apagar_bomba();
-    this->activar_espera_fija();
-    this->action = climate::CLIMATE_ACTION_OFF; // Actualizar la acción a "OFF"
-    this->publish_state();
-  }
-}
-
-bool CustomClimate::diferencia_temperatura_suficiente() {
-  float diferencia_media = this->diferencia_media_number_ != nullptr ? this->diferencia_media_number_->state : this->diferencia_media_;
-  float temp_agua = this->get_current_temperature();
-
-  return (temp_agua < this->target_temperature - diferencia_media);
-}
-
-void CustomClimate::encender_bomba() {
-  this->log_mensaje("WARN", "Encendiendo bomba");
-  if (this->interruptor_bomba_ != nullptr) {
-    this->interruptor_bomba_->turn_on();
-    this->conteo_encendidos_++;
-    this->tiempo_inicio_ = this->obtener_tiempo_actual();
-  } else {
-    this->log_mensaje("ERROR", "Interruptor de bomba no configurado");
-  }
-}
-
-void CustomClimate::apagar_bomba() {
-  this->log_mensaje("WARN", "Apagando bomba");
-  if (this->interruptor_bomba_ != nullptr) {
-    this->interruptor_bomba_->turn_off();
-    this->actualizar_consumo();
-  } else {
-    this->log_mensaje("ERROR", "Interruptor de bomba no configurado");
-  }
-}
-
-void CustomClimate::esperar_estabilizacion() {
-  this->log_mensaje("DEBUG", "Iniciando estabilización");
-  this->estabilizando_ = true;
-  this->tiempo_estabilizacion_inicio_ = millis();
-}
-
-void CustomClimate::activar_espera_proporcional() {
-  float diferencia_temp = this->target_temperature - this->current_temperature;
-  if (diferencia_temp > this->temperatura_cerca_) {
-    // Si la diferencia de temperatura es mayor que temperatura_cerca, se activa el control normal
-    this->control_bomba_normal();
-    return;
-  }
-
-  int tiempo_activacion = static_cast<int>(diferencia_temp * this->factor_tiempo_activacion_);
-  this->espera_ = true;
-  this->tiempo_espera_fin_ = this->obtener_tiempo_actual() + tiempo_activacion;
-  this->log_mensaje("DEBUG", "Activada espera proporcional por %d segundos", tiempo_activacion);
-}
-
-void CustomClimate::activar_espera_fija() {
-  this->espera_ = true;
-  this->tiempo_espera_fin_ = this->obtener_tiempo_actual() + 180;  // 3 minutos
-  this->log_mensaje("DEBUG", "Activada espera fija por 3 minutos");
-}
-
-bool CustomClimate::temperatura_alcanzada() {
-  return (this->current_temperature >= this->target_temperature);
 }
 
 int64_t CustomClimate::obtener_tiempo_actual() {
@@ -320,14 +273,6 @@ void CustomClimate::log_mensaje(const char* nivel, const char* formato, ...) {
   } else if (strcmp(nivel, "VERBOSE") == 0) {
     ESP_LOGV(TAG, "%s", buffer);
   }
-}
-
-String CustomClimate::formatear_tiempo_espera(int64_t segundos) {
-  int minutos = segundos / 60;
-  int segundos_restantes = segundos % 60;
-  char buffer[20];
-  snprintf(buffer, sizeof(buffer), "%02d:%02d", minutos, segundos_restantes);
-  return String(buffer);
 }
 
 }  // namespace custom_climate
