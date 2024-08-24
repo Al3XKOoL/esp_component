492a924a5691793922d48d13256adf7bb7975741
Al3XKOoL <moonrotation@gmail.com>
2024-08-24 19:58:44 +0100

    remove spi

--- esphome/components/ili9xxx_8080/ili9xxx_display.cpp
+++ esphome/components/ili9xxx_8080/ili9xxx_display.cpp
@@ -1,3 +1,4 @@
+// ili9xxx_display.cpp
 #include "ili9xxx_display.h"
 #include "esphome/core/application.h"
 #include "esphome/core/hal.h"
@@ -7,28 +8,8 @@
 namespace esphome {
 namespace ili9xxx {
 
-static const uint16_t SPI_SETUP_US = 100;         // estimated fixed overhead in microseconds for an SPI write
-static const uint16_t SPI_MAX_BLOCK_SIZE = 4092;  // Max size of continuous SPI transfer
-
-// store a 16 bit value in a buffer, big endian.
-static inline void put16_be(uint8_t *buf, uint16_t value) {
-  buf[0] = value >> 8;
-  buf[1] = value;
-}
-
-void ILI9XXXDisplay::set_madctl() {
-  // custom x/y transform and color order
-  uint8_t mad = this->color_order_ == display::COLOR_ORDER_BGR ? MADCTL_BGR : MADCTL_RGB;
-  if (this->swap_xy_)
-    mad |= MADCTL_MV;
-  if (this->mirror_x_)
-    mad |= MADCTL_MX;
-  if (this->mirror_y_)
-    mad |= MADCTL_MY;
-  this->command(ILI9XXX_MADCTL);
-  this->data(mad);
-  esph_log_d(TAG, "Wrote MADCTL 0x%02X", mad);
-}
+static const uint16_t PARALLEL_SETUP_US = 100; // estimated fixed overhead in microseconds for a parallel write
+static const uint16_t PARALLEL_MAX_BLOCK_SIZE = 4092; // Max size of continuous parallel transfer
 
 void ILI9XXXDisplay::setup() {
   ESP_LOGD(TAG, "Setting up ILI9xxx");
@@ -83,13 +64,6 @@ void ILI9XXXDisplay::alloc_buffer_() {
   }
 }
 
-
-      this->x_high_ = x;
-    if (y > this->y_high_)
-      this->y_high_ = y;
-  }
-}
-
 void ILI9XXXDisplay::update() {
   if (this->prossing_update_) {
     this->need_update_ = true;
@@ -116,9 +90,9 @@ void ILI9XXXDisplay::display_() {
 
   size_t mhz = this->data_rate_ / 1000000;
   // estimate time for a single write
-  size_t sw_time = this->width_ * h * 16 / mhz + this->width_ * h * 2 / SPI_MAX_BLOCK_SIZE * SPI_SETUP_US * 2;
+  size_t sw_time = this->width_ * h * 16 / mhz + this->width_ * h * 2 / PARALLEL_MAX_BLOCK_SIZE * PARALLEL_SETUP_US * 2;
   // estimate time for multiple writes
-  size_t mw_time = (w * h * 16) / mhz + w * h * 2 / ILI9XXX_TRANSFER_BUFFER_SIZE * SPI_SETUP_US;
+  size_t mw_time = (w * h * 16) / mhz + w * h * 2 / PARALLEL_MAX_BLOCK_SIZE * PARALLEL_SETUP_US;
   ESP_LOGV(TAG,
            "Start display(xlow:%d, ylow:%d, xhigh:%d, yhigh:%d, width:%d, "
            "height:%zu, mode=%d, 18bit=%d, sw_time=%zuus, mw_time=%zuus)",
@@ -132,11 +106,11 @@ void ILI9XXXDisplay::display_() {
     this->write_array(this->buffer_ + this->y_low_ * this->width_ * 2, h * this->width_ * 2);
   } else {
     ESP_LOGV(TAG, "Doing multiple write");
-    uint8_t transfer_buffer[ILI9XXX_TRANSFER_BUFFER_SIZE];
-    size_t rem = h * w;  // remaining number of pixels to write
+    uint8_t transfer_buffer[PARALLEL_MAX_BLOCK_SIZE];
+    size_t rem = h * w; // remaining number of pixels to write
     set_addr_window_(this->x_low_, this->y_low_, this->x_high_, this->y_high_);
-    size_t idx = 0;    // index into transfer_buffer
-    size_t pixel = 0;  // pixel number offset
+    size_t idx = 0; // index into transfer_buffer
+    size_t pixel = 0; // pixel number offset
     size_t pos = this->y_low_ * this->width_ + this->x_low_;
     while (rem-- != 0) {
       uint16_t color_val;
@@ -148,18 +122,18 @@ void ILI9XXXDisplay::display_() {
           color_val = display::ColorUtil::color_to_565(
               display::ColorUtil::index8_to_color_palette888(this->buffer_[pos++], this->palette_));
           break;
-        default:  // case BITS_16:
+        default: // case BITS_16:
           color_val = (this->buffer_[pos * 2] << 8) + this->buffer_[pos * 2 + 1];
           pos++;
           break;
       }
       if (this->is_18bitdisplay_) {
-        transfer_buffer[idx++] = (uint8_t) ((color_val & 0xF800) >> 8);  // Blue
-        transfer_buffer[idx++] = (uint8_t) ((color_val & 0x7E0) >> 3);   // Green
-        transfer_buffer[idx++] = (uint8_t) (color_val << 3);             // Red
+        transfer_buffer[idx++] = (uint8_t)((color_val & 0xF800) >> 8); // Blue
+        transfer_buffer[idx++] = (uint8_t)((color_val & 0x7E0) >> 3); // Green
+        transfer_buffer[idx++] = (uint8_t)(color_val << 3); // Red
       } else {
-        put16_be(transfer_buffer + idx, color_val);
-        idx += 2;
+        transfer_buffer[idx++] = (color_val >> 8);
+        transfer_buffer[idx++] = (color_val & 0xFF);
       }
       if (idx == sizeof(transfer_buffer)) {
         this->write_array(transfer_buffer, idx);
@@ -178,7 +152,7 @@ void ILI9XXXDisplay::display_() {
     }
   }
   this->end_data_();
-  ESP_LOGV(TAG, "Data write took %dms", (unsigned) (millis() - now));
+  ESP_LOGV(TAG, "Data write took %dms", (unsigned)(millis() - now));
   // invalidate watermarks
   this->x_low_ = this->width_;
   this->y_low_ = this->height_;
@@ -186,7 +160,6 @@ void ILI9XXXDisplay::display_() {
   this->y_high_ = 0;
 }
 
-// note that this bypasses the buffer and writes directly to the display.
 void ILI9XXXDisplay::draw_pixels_at(int x_start, int y_start, int w, int h, const uint8_t *ptr,
                                     display::ColorOrder order, display::ColorBitness bitness, bool big_endian,
                                     int x_offset, int y_offset, int x_pad) {
@@ -213,18 +186,18 @@ void ILI9XXXDisplay::draw_pixels_at(int x_start, int y_start, int w, int h, cons
     }
   } else {
     // 18 bit mode
-    uint8_t transfer_buffer[ILI9XXX_TRANSFER_BUFFER_SIZE * 4];
+    uint8_t transfer_buffer[PARALLEL_MAX_BLOCK_SIZE * 4];
     ESP_LOGV(TAG, "Doing multiple write");
-    size_t rem = h * w;  // remaining number of pixels to write
-    size_t idx = 0;      // index into transfer_buffer
-    size_t pixel = 0;    // pixel number offset
+    size_t rem = h * w; // remaining number of pixels to write
+    size_t idx = 0; // index into transfer_buffer
+    size_t pixel = 0; // pixel number offset
     ptr += (y_offset * stride + x_offset) * 2;
     while (rem-- != 0) {
       uint8_t hi_byte = *ptr++;
       uint8_t lo_byte = *ptr++;
-      transfer_buffer[idx++] = hi_byte & 0xF8;                     // Blue
-      transfer_buffer[idx++] = ((hi_byte << 5) | (lo_byte) >> 5);  // Green
-      transfer_buffer[idx++] = lo_byte << 3;                       // Red
+      transfer_buffer[idx++] = hi_byte & 0xF8; // Blue
+      transfer_buffer[idx++] = ((hi_byte << 5) | (lo_byte) >> 5); // Green
+      transfer_buffer[idx++] = lo_byte << 3; // Red
       if (idx == sizeof(transfer_buffer)) {
         this->write_array(transfer_buffer, idx);
         idx = 0;
@@ -244,47 +217,38 @@ void ILI9XXXDisplay::draw_pixels_at(int x_start, int y_start, int w, int h, cons
   this->end_data_();
 }
 
-// should return the total size: return this->get_width_internal() * this->get_height_internal() * 2 // 16bit color
-// values per bit is huge
-uint32_t ILI9XXXDisplay::get_buffer_length_() { return this->get_width_internal() * this->get_height_internal(); }
-
 void ILI9XXXDisplay::command(uint8_t value) {
-  this->start_command_();
-  this->write_byte(value);
-  this->end_command_();
+  if (this->interface_mode_ == PARALLEL_8BIT_MODE) {
+    this->wr_pin_->digital_write(false); // Set W/R pin to low for write
+    for (int i = 0; i < 8; i++) {
+      this->data_pins_[i]->digital_write((value >> i) & 1);
+    }
+    this->wr_pin_->digital_write(true); // Pulse W/R pin high to latch data
+    this->wr_pin_->digital_write(false); // Set W/R pin to low for command mode
+    this->data_pins_->digital_write(ILI9XXX_CMD_MODE); // Set data pins to command mode
+    this->wr_pin_->digital_write(true); // Pulse W/R pin high to latch command mode
+  }
 }
 
 void ILI9XXXDisplay::data(uint8_t value) {
-  this->start_data_();
-  this->write_byte(value);
-  this->end_data_();
+  if (this->interface_mode_ == PARALLEL_8BIT_MODE) {
+    this->wr_pin_->digital_write(false); // Set W/R pin to low for write
+    for (int i = 0; i < 8; i++) {
+      this->data_pins_[i]->digital_write((value >> i) & 1);
+    }
+    this->wr_pin_->digital_write(true); // Pulse W/R pin high to latch data
+    this->wr_pin_->digital_write(false); // Set W/R pin to low for data mode
+    this->data_pins_->digital_write(ILI9XXX_DATA_MODE); // Set data pins to data mode
+    this->wr_pin_->digital_write(true); // Pulse W/R pin high to latch data mode
+  }
 }
 
 void ILI9XXXDisplay::send_command(uint8_t command_byte, const uint8_t *data_bytes, uint8_t num_data_bytes) {
-  this->command(command_byte);  // Send the command byte
-  this->start_data_();
-  this->write_array(data_bytes, num_data_bytes);
-  this->end_data_();
-}
-
-void ILI9XXXDisplay::start_command_() {
-  this->dc_pin_->digital_write(false);
-  this->enable();
-}
-void ILI9XXXDisplay::start_data_() {
-  this->dc_pin_->digital_write(true);
-  this->enable();
-}
-
-void ILI9XXXDisplay::end_command_() { this->disable(); }
-void ILI9XXXDisplay::end_data_() { this->disable(); }
-
-void ILI9XXXDisplay::reset_() {
-  if (this->reset_pin_ != nullptr) {
-    this->reset_pin_->digital_write(false);
-    delay(20);
-    this->reset_pin_->digital_write(true);
-    delay(20);
+  if (this->interface_mode_ == PARALLEL_8BIT_MODE) {
+    this->command(command_byte);
+    for (int i = 0; i < num_data_bytes; i++) {
+      this->data(data_bytes[i]);
+    }
   }
 }
 
@@ -305,13 +269,12 @@ void ILI9XXXDisplay::init_lcd_(const uint8_t *addr) {
       addr += num_args;
       if (x & 0x80) {
         ESP_LOGV(TAG, "Delay 150ms");
-        delay(150);  // NOLINT
+        delay(150); // NOLINT
       }
     }
   }
 }
 
-// Tell the display controller where we want to draw pixels.
 void ILI9XXXDisplay::set_addr_window_(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2) {
   x1 += this->offset_x_;
   x2 += this->offset_x_;
@@ -322,12 +285,12 @@ void ILI9XXXDisplay::set_addr_window_(uint16_t x1, uint16_t y1, uint16_t x2, uin
   this->data(x1 & 0xFF);
   this->data(x2 >> 8);
   this->data(x2 & 0xFF);
-  this->command(ILI9XXX_PASET);  // Page address set
+  this->command(ILI9XXX_PASET); // Page address set
   this->data(y1 >> 8);
   this->data(y1 & 0xFF);
   this->data(y2 >> 8);
   this->data(y2 & 0xFF);
-  this->command(ILI9XXX_RAMWR);  // Write to RAM
+  this->command(ILI9XXX_RAMWR); // Write to RAM
   this->start_data_();
 }
 
@@ -341,5 +304,26 @@ void ILI9XXXDisplay::invert_colors(bool invert) {
 int ILI9XXXDisplay::get_width_internal() { return this->width_; }
 int ILI9XXXDisplay::get_height_internal() { return this->height_; }
 
-}  // namespace ili9xxx
-}  // namespace esphome
+void ILI9XXXDisplay::reset_() {
+  if (this->reset_pin_ != nullptr) {
+    this->reset_pin_->digital_write(false);
+    delay(20);
+    this->reset_pin_->digital_write(true);
+    delay(20);
+  }
+}
+
+uint32_t ILI9XXXDisplay::get_buffer_length_() { return this->get_width_internal() * this->get_height_internal(); }
+
+void ILI9XXXDisplay::write_array(const uint8_t *data, size_t len) {
+  for (size_t i = 0; i < len; i++) {
+    this->data(data[i]);
+  }
+}
+
+void ILI9XXXDisplay::write_byte(uint8_t value) {
+  this->data(value);
+}
+
+} // namespace ili9xxx
+} // namespace esphome
