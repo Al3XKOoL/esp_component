3fb8f6e814ad6960f5384ef5d3cc77bcd365b98b
Al3XKOoL <moonrotation@gmail.com>
2024-08-24 21:05:18 +0100

    Update ili9xxx_display.h

--- esphome/components/ili9xxx_8080/ili9xxx_display.h
+++ esphome/components/ili9xxx_8080/ili9xxx_display.h
@@ -6,27 +6,36 @@
 namespace esphome {
 namespace ili9xxx {
 
-class ILI9341ParallelDisplay : public display::DisplayBuffer {
+class ILI9341ParallelDisplay : public display::DisplayBuffer, public Component {
  public:
   void setup() override;
   void dump_config() override;
+  void display();
   
   void set_dc_pin(GPIOPin *pin) { dc_pin_ = pin; }
   void set_reset_pin(GPIOPin *pin) { reset_pin_ = pin; }
   void set_wr_pin(GPIOPin *pin) { wr_pin_ = pin; }
   void set_rd_pin(GPIOPin *pin) { rd_pin_ = pin; }
   void set_data_pin(uint8_t index, GPIOPin *pin) { data_pins_[index] = pin; }
+  void set_dimensions(uint16_t width, uint16_t height) {
+    width_ = width;
+    height_ = height;
+  }
 
  protected:
+  void draw_absolute_pixel_internal(int x, int y, Color color) override;
   void write_command(uint8_t command);
   void write_data(uint8_t data);
   void write_array(const uint8_t *data, size_t len);
+  void init_display();
 
   GPIOPin *dc_pin_{nullptr};
   GPIOPin *reset_pin_{nullptr};
   GPIOPin *wr_pin_{nullptr};
   GPIOPin *rd_pin_{nullptr};
   GPIOPin *data_pins_[8]{nullptr};
+  uint16_t width_{240};
+  uint16_t height_{320};
 };
 
 }  // namespace ili9xxx
