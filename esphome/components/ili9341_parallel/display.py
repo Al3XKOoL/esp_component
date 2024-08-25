7f0ebc73a7d9f5e725fb11be4e3f83f1d477c551
Al3XKOoL <moonrotation@gmail.com>
2024-08-25 12:21:01 +0100

    Create display.py

---
+++ esphome/components/ili9341_parallel/display.py
@@ -0,0 +1,61 @@
+# esphome/components/ili9341_parallel/display.py
+
+import esphome.codegen as cg
+from esphome import config_validation as cv
+from esphome.components import display
+from esphome.const import CONF_ID, CONF_UPDATE_INTERVAL, CONF_MODEL
+
+DEPENDENCIES = ['spi']
+
+# Configuración del componente
+CONF_CS_PIN = "cs_pin"
+CONF_DC_PIN = "dc_pin"
+CONF_RESET_PIN = "reset_pin"
+CONF_D0_PIN = "d0_pin"
+CONF_D1_PIN = "d1_pin"
+CONF_D2_PIN = "d2_pin"
+CONF_D3_PIN = "d3_pin"
+CONF_D4_PIN = "d4_pin"
+CONF_D5_PIN = "d5_pin"
+CONF_D6_PIN = "d6_pin"
+CONF_D7_PIN = "d7_pin"
+
+ili9341_parallel_ns = cg.esphome_ns.namespace('ili9341_parallel')
+ILI9341ParallelDisplay = ili9341_parallel_ns.class_('ILI9341ParallelDisplay', display.DisplayBuffer)
+
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(ILI9341ParallelDisplay),
+    cv.Required(CONF_CS_PIN): cv.positive_int,
+    cv.Required(CONF_DC_PIN): cv.positive_int,
+    cv.Required(CONF_RESET_PIN): cv.positive_int,
+    cv.Required(CONF_D0_PIN): cv.positive_int,
+    cv.Required(CONF_D1_PIN): cv.positive_int,
+    cv.Required(CONF_D2_PIN): cv.positive_int,
+    cv.Required(CONF_D3_PIN): cv.positive_int,
+    cv.Required(CONF_D4_PIN): cv.positive_int,
+    cv.Required(CONF_D5_PIN): cv.positive_int,
+    cv.Required(CONF_D6_PIN): cv.positive_int,
+    cv.Required(CONF_D7_PIN): cv.positive_int,
+    cv.Optional(CONF_UPDATE_INTERVAL, default="1s"): cv.positive_time_period_milliseconds,
+    cv.Optional(CONF_MODEL, default="ILI9341"): cv.string,
+}).extend(cv.COMPONENT_SCHEMA).extend(display.FANOUT_SCHEMA)
+
+def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    cg.add(var.set_cs_pin(config[CONF_CS_PIN]))
+    cg.add(var.set_dc_pin(config[CONF_DC_PIN]))
+    cg.add(var.set_reset_pin(config[CONF_RESET_PIN]))
+    cg.add(var.set_d0_pin(config[CONF_D0_PIN]))
+    cg.add(var.set_d1_pin(config[CONF_D1_PIN]))
+    cg.add(var.set_d2_pin(config[CONF_D2_PIN]))
+    cg.add(var.set_d3_pin(config[CONF_D3_PIN]))
+    cg.add(var.set_d4_pin(config[CONF_D4_PIN]))
+    cg.add(var.set_d5_pin(config[CONF_D5_PIN]))
+    cg.add(var.set_d6_pin(config[CONF_D6_PIN]))
+    cg.add(var.set_d7_pin(config[CONF_D7_PIN]))
+    if CONF_UPDATE_INTERVAL in config:
+        cg.add(var.set_update_interval(config[CONF_UPDATE_INTERVAL]))
+    if CONF_MODEL in config:
+        cg.add(var.set_model(config[CONF_MODEL]))
+    yield cg.register_component(var, config)
+    yield from display.build_display(var, config)
