9b61cd3b703a4fd52510ccef05503d21b4968f7f
Al3XKOoL <moonrotation@gmail.com>
2024-08-25 20:23:35 +0100

    Update tft_espi.h

--- esphome/components/tft_espi/tft_espi.h
+++ esphome/components/tft_espi/tft_espi.h
@@ -13,12 +13,14 @@ class TFTeSPIDisplay : public display::DisplayBuffer {
   void setup() override;
   void dump_config() override;
   float get_setup_priority() const override;
-  void display() override;  // Asegúrate de que este método exista en la clase base
-  void set_brightness(float brightness) override;  // Asegúrate de que este método exista en la clase base
+  void display();  // Revisa si override es necesario
+  void set_brightness(float brightness);  // Revisa si override es necesario
   void fill(Color color);
   void draw_absolute_pixel_internal(int x, int y, Color color) override;
   int get_width_internal() override;
   int get_height_internal() override;
+  Color get_pixel_color(int x, int y);  // Agrega esta línea si es necesario
+  esphome::display::DisplayType get_display_type() override;  // Si es necesario
 
  protected:
   void update();  // Implementa este método si es necesario
