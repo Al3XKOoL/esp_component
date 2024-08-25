058126087c536b1fb7f2b0a58f94a25b2cfb06e2
Al3XKOoL <moonrotation@gmail.com>
2024-08-25 15:24:24 +0100

    Update display.py

--- esphome/components/tft_espi/display.py
+++ esphome/components/tft_espi/display.py
@@ -10,8 +10,11 @@ TFTeSPIDisplay = tft_espi_ns.class_('TFTeSPIDisplay', display.Display)
 
 CONFIG_SCHEMA = display.BASIC_DISPLAY_SCHEMA.extend({
     cv.GenerateID(): cv.declare_id(TFTeSPIDisplay),
+    cv.Optional('auto_clear_enabled', default=True): cv.boolean,
 }).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await display.register_display(var, config)
+    if 'auto_clear_enabled' in config:
+        cg.add(var.set_auto_clear(config['auto_clear_enabled']))
