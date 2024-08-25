d2c34578257a562e16b5f6f4e41dd231632699bf
Al3XKOoL <moonrotation@gmail.com>
2024-08-25 12:29:21 +0100

    Update ili9431_parallel.cpp

--- esphome/components/ili9341_parallel/ili9431_parallel.cpp
+++ esphome/components/ili9341_parallel/ili9431_parallel.cpp
@@ -1,9 +1,29 @@
+// esphome/components/ili9341_parallel/ili9341_parallel.cpp
+
 #include "ili9341_parallel.h"
+#include "esphome/core/log.h"
 
 namespace esphome {
 namespace ili9341_parallel {
 
-// Implement any additional methods if needed
+static const char *const TAG = "ili9341_parallel";
+
+void ILI9341ParallelDisplay::setup() {
+  // Initialization code for the display
+  ESP_LOGD(TAG, "Setting up ILI9341 Parallel Display");
+}
+
+void ILI9341ParallelDisplay::loop() {
+  // Main loop code if needed
+}
+
+void ILI9341ParallelDisplay::dump_config() {
+  ESP_LOGCONFIG(TAG, "ILI9341 Parallel Display:");
+  ESP_LOGCONFIG(TAG, "  CS Pin: %d", cs_pin_->get_pin());
+  ESP_LOGCONFIG(TAG, "  DC Pin: %d", dc_pin_->get_pin());
+  ESP_LOGCONFIG(TAG, "  Reset Pin: %d", reset_pin_->get_pin());
+  // Log the rest of the pins
+}
 
 }  // namespace ili9341_parallel
 }  // namespace esphome
