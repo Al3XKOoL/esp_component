a3609a23dd380c1c5040180b3ee33c6d6a18888c
Al3XKOoL <moonrotation@gmail.com>
2024-08-25 13:49:35 +0100

    Create tft_espi_component.h

---
+++ esphome/components/tft_espi/tft_espi_component.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "esphome/core/component.h"
+#include "esphome/components/display/display_buffer.h"
+#include "TFT_eSPI.h"
+
+namespace esphome {
+namespace tft_espi {
+
+class TFT_eSPIComponent : public esphome::Component, public esphome::display::DisplayBuffer {
+public:
+  TFT_eSPIComponent(TFT_eSPI* tft) : tft_(tft) {}
+
+  void setup() override {
+    tft_->begin();
+    tft_->setRotation(rotation_);
+    tft_->fillScreen(TFT_BLACK);
+  }
+
+  void draw_pixel(int x, int y, color_t color) override {
+    tft_->drawPixel(x, y, color);
+  }
+
+  // Agrega métodos para otras operaciones de la pantalla según lo necesites.
+
+private:
+  TFT_eSPI* tft_;
+  int rotation_ = 0;
+};
+
+}  // namespace tft_espi
+}  // namespace esphome
