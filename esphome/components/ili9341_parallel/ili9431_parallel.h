e5b3d884c440f0de4a55e3b6e31def51865c6dfc
Al3XKOoL <moonrotation@gmail.com>
2024-08-25 13:17:10 +0100

    Update ili9431_parallel.h

--- esphome/components/ili9341_parallel/ili9431_parallel.h
+++ esphome/components/ili9341_parallel/ili9431_parallel.h
@@ -8,30 +8,26 @@ namespace esphome {
 namespace ili9341_parallel {
 
 class ILI9341ParallelDisplay : public Component, public display::DisplayBuffer {
- public:
-  void set_model(const std::string &model) { model_ = model; }
-  void set_width(int16_t width) { width_ = width; }
-  void set_height(int16_t height) { height_ = height; }
-  void set_dc_pin(GPIOPin *pin) { dc_pin_ = pin; }
-  void set_reset_pin(GPIOPin *pin) { reset_pin_ = pin; }
-  void set_wr_pin(GPIOPin *pin) { wr_pin_ = pin; }
-  void set_rd_pin(GPIOPin *pin) { rd_pin_ = pin; }
-  void set_data_pin(int index, GPIOPin *pin) { data_pins_[index] = pin; }
+public:
+    void set_model(const std::string &model) { model_ = model; }
+    void set_dc_pin(GPIOPin *pin) { dc_pin_ = pin; }
+    void set_reset_pin(GPIOPin *pin) { reset_pin_ = pin; }
+    void set_wr_pin(GPIOPin *pin) { wr_pin_ = pin; }
+    void set_rd_pin(GPIOPin *pin) { rd_pin_ = pin; }
+    void set_data_pin(int index, GPIOPin *pin) { data_pins_[index] = pin; }
 
-  void setup() override;
-  void dump_config() override;
-  float get_setup_priority() const override { return setup_priority::PROCESSOR; }
-  void display();
+    void setup() override;
+    void dump_config() override;
+    float get_setup_priority() const override { return setup_priority::PROCESSOR; }
+    void display();
 
- protected:
-  std::string model_;
-  int16_t width_{240};
-  int16_t height_{320};
-  GPIOPin *dc_pin_{nullptr};
-  GPIOPin *reset_pin_{nullptr};
-  GPIOPin *wr_pin_{nullptr};
-  GPIOPin *rd_pin_{nullptr};
-  GPIOPin *data_pins_[8]{nullptr};
+protected:
+    std::string model_;
+    GPIOPin *dc_pin_{nullptr};
+    GPIOPin *reset_pin_{nullptr};
+    GPIOPin *wr_pin_{nullptr};
+    GPIOPin *rd_pin_{nullptr};
+    GPIOPin *data_pins_[8]{nullptr};
 };
 
 }  // namespace ili9341_parallel
