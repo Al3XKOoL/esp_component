6f27c0ce189a1f7336f95123585d12cbc1ea1125
Al3XKOoL <moonrotation@gmail.com>
2024-08-25 12:10:56 +0100

    Update ili9431_parallel.h

--- esphome/components/ili9341_parallel/ili9431_parallel.h
+++ esphome/components/ili9341_parallel/ili9431_parallel.h
@@ -1,18 +1,20 @@
 #include "esphome.h"
 #include "TFT_eSPI.h"
 
-class ILI9341ParallelDisplay : public Component, public DisplayBuffer {
+namespace esphome {
+namespace ili9341_parallel {
+
+class ILI9341ParallelDisplay : public Component, public display::DisplayBuffer {
  public:
-  TFT_eSPI tft = TFT_eSPI();  // Configura TFT_eSPI en modo paralelo
+  TFT_eSPI tft = TFT_eSPI();
 
   void setup() override {
     tft.init();
-    tft.setRotation(0);  // Ajusta la rotación
+    tft.setRotation(rotation_);
   }
 
-  void update() override {
-    tft.fillScreen(TFT_BLACK);
-    tft.drawString("Hello, ESPHome!", 10, 10, 2);
+  void loop() override {
+    this->do_update_();
   }
 
   void draw_absolute_pixel_internal(int x, int y, Color color) override {
@@ -22,4 +24,12 @@ class ILI9341ParallelDisplay : public Component, public DisplayBuffer {
 
   int get_width_internal() override { return 240; }
   int get_height_internal() override { return 320; }
+
+  void set_rotation(uint8_t rotation) { this->rotation_ = rotation; }
+
+ protected:
+  uint8_t rotation_;
 };
+
+}  // namespace ili9341_parallel
+}  // namespace esphome
