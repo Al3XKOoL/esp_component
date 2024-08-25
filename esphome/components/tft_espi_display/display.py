4846497faf626cf94394041de5a0508ebfaa31c5
Al3XKOoL <moonrotation@gmail.com>
2024-08-25 19:43:58 +0100

    Update display.py

--- esphome/components/tft_espi_display/display.py
+++ esphome/components/tft_espi_display/display.py
@@ -1,28 +1,27 @@
+import esphome.components.tft_espi as tft_espi
 import esphome.codegen as cg
 import esphome.config_validation as cv
-import esphome.components.tft_espi as tft_espi
 from esphome.components import display
 from esphome.const import CONF_ID, CONF_LAMBDA, CONF_ROTATION, CONF_INVERT
 
-DEPENDENCIES = ['tft_espi']
-
+# Crea un namespace para tu componente
 tft_espi_ns = cg.esphome_ns.namespace('tft_espi')
 TftEspiComponent = tft_espi_ns.class_('TftEspiComponent', display.DisplayBuffer, cg.Component)
 
+# Esquema de configuración para el YAML
 CONFIG_SCHEMA = display.FULL_DISPLAY_SCHEMA.extend({
     cv.Required(CONF_ID): cv.declare_id(TftEspiComponent),
     cv.Optional(CONF_ROTATION, default=0): cv.int_range(min=0, max=3),
     cv.Optional(CONF_INVERT, default=False): cv.boolean,
 }).extend(cv.COMPONENT_SCHEMA)
 
+# Función que traduce el YAML a código C++
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
 
-    # Configurar rotación
+    # Configurar rotación e inversión
     if CONF_ROTATION in config:
         cg.add(var.set_rotation(config[CONF_ROTATION]))
-
-    # Configurar inversión de colores
     if CONF_INVERT in config:
         cg.add(var.set_invert(config[CONF_INVERT]))
 
