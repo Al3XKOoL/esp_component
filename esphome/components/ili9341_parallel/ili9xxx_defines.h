fc464931bfc7e45a2501061993eac7ee83902072
Al3XKOoL <moonrotation@gmail.com>
2024-08-25 08:24:07 +0100

    Update ili9xxx_defines.h

--- esphome/components/ili9341_parallel/ili9xxx_defines.h
+++ esphome/components/ili9341_parallel/ili9xxx_defines.h
@@ -15,99 +15,99 @@ static const uint8_t MADCTL_MH    = 0x04;   ///< Bit 2: LCD refresh right to lef
 // clang-format on
 
 // ILI9XXX specific commands
-static const uint8_t ILI9XXX_NOP = 0x00;
-static const uint8_t ILI9XXX_SWRESET = 0x01;
-static const uint8_t ILI9XXX_RDDID = 0x04;
-static const uint8_t ILI9XXX_RDDST = 0x09;
+static const uint8_t ILI9XXX_NOP       = 0x00;
+static const uint8_t ILI9XXX_SWRESET   = 0x01;
+static const uint8_t ILI9XXX_RDDID     = 0x04;
+static const uint8_t ILI9XXX_RDDST     = 0x09;
 
-static const uint8_t ILI9XXX_SLPIN = 0x10;
-static const uint8_t ILI9XXX_SLPOUT = 0x11;
-static const uint8_t ILI9XXX_PTLON = 0x12;
-static const uint8_t ILI9XXX_NORON = 0x13;
+static const uint8_t ILI9XXX_SLPIN     = 0x10;
+static const uint8_t ILI9XXX_SLPOUT    = 0x11;
+static const uint8_t ILI9XXX_PTLON     = 0x12;
+static const uint8_t ILI9XXX_NORON     = 0x13;
 
-static const uint8_t ILI9XXX_RDMODE = 0x0A;
-static const uint8_t ILI9XXX_RDMADCTL = 0x0B;
-static const uint8_t ILI9XXX_RDPIXFMT = 0x0C;
-static const uint8_t ILI9XXX_RDIMGFMT = 0x0D;
-static const uint8_t ILI9XXX_RDSELFDIAG = 0x0F;
+static const uint8_t ILI9XXX_RDMODE    = 0x0A;
+static const uint8_t ILI9XXX_RDMADCTL  = 0x0B;
+static const uint8_t ILI9XXX_RDPIXFMT  = 0x0C;
+static const uint8_t ILI9XXX_RDIMGFMT  = 0x0D;
+static const uint8_t ILI9XXX_RDSELFDIAG= 0x0F;
 
-static const uint8_t ILI9XXX_INVOFF = 0x20;
-static const uint8_t ILI9XXX_INVON = 0x21;
-static const uint8_t ILI9XXX_GAMMASET = 0x26;
-static const uint8_t ILI9XXX_DISPOFF = 0x28;
-static const uint8_t ILI9XXX_DISPON = 0x29;
+static const uint8_t ILI9XXX_INVOFF    = 0x20;
+static const uint8_t ILI9XXX_INVON     = 0x21;
+static const uint8_t ILI9XXX_GAMMASET  = 0x26;
+static const uint8_t ILI9XXX_DISPOFF   = 0x28;
+static const uint8_t ILI9XXX_DISPON    = 0x29;
 
-static const uint8_t ILI9XXX_CASET = 0x2A;
-static const uint8_t ILI9XXX_PASET = 0x2B;
-static const uint8_t ILI9XXX_RAMWR = 0x2C;
-static const uint8_t ILI9XXX_RAMRD = 0x2E;
+static const uint8_t ILI9XXX_CASET     = 0x2A;
+static const uint8_t ILI9XXX_PASET     = 0x2B;
+static const uint8_t ILI9XXX_RAMWR     = 0x2C;
+static const uint8_t ILI9XXX_RAMRD     = 0x2E;
 
-static const uint8_t ILI9XXX_PTLAR = 0x30;
-static const uint8_t ILI9XXX_VSCRDEF = 0x33;
-static const uint8_t ILI9XXX_MADCTL = 0x36;
-static const uint8_t ILI9XXX_VSCRSADD = 0x37;
-static const uint8_t ILI9XXX_IDMOFF = 0x38;
-static const uint8_t ILI9XXX_IDMON = 0x39;
-static const uint8_t ILI9XXX_PIXFMT = 0x3A;
-static const uint8_t ILI9XXX_COLMOD = 0x3A;
+static const uint8_t ILI9XXX_PTLAR     = 0x30;
+static const uint8_t ILI9XXX_VSCRDEF   = 0x33;
+static const uint8_t ILI9XXX_MADCTL    = 0x36;
+static const uint8_t ILI9XXX_VSCRSADD  = 0x37;
+static const uint8_t ILI9XXX_IDMOFF    = 0x38;
+static const uint8_t ILI9XXX_IDMON     = 0x39;
+static const uint8_t ILI9XXX_PIXFMT    = 0x3A;
+static const uint8_t ILI9XXX_COLMOD    = 0x3A;
 
 static const uint8_t ILI9XXX_GETSCANLINE = 0x45;
 
-static const uint8_t ILI9XXX_WRDISBV = 0x51;
-static const uint8_t ILI9XXX_RDDISBV = 0x52;
-static const uint8_t ILI9XXX_WRCTRLD = 0x53;
+static const uint8_t ILI9XXX_WRDISBV   = 0x51;
+static const uint8_t ILI9XXX_RDDISBV   = 0x52;
+static const uint8_t ILI9XXX_WRCTRLD   = 0x53;
 
-static const uint8_t ILI9XXX_IFMODE = 0xB0;
-static const uint8_t ILI9XXX_FRMCTR1 = 0xB1;
-static const uint8_t ILI9XXX_FRMCTR2 = 0xB2;
-static const uint8_t ILI9XXX_FRMCTR3 = 0xB3;
-static const uint8_t ILI9XXX_INVCTR = 0xB4;
-static const uint8_t ILI9XXX_DFUNCTR = 0xB6;
-static const uint8_t ILI9XXX_ETMOD = 0xB7;
+static const uint8_t ILI9XXX_IFMODE    = 0xB0;
+static const uint8_t ILI9XXX_FRMCTR1   = 0xB1;
+static const uint8_t ILI9XXX_FRMCTR2   = 0xB2;
+static const uint8_t ILI9XXX_FRMCTR3   = 0xB3;
+static const uint8_t ILI9XXX_INVCTR    = 0xB4;
+static const uint8_t ILI9XXX_DFUNCTR   = 0xB6;
+static const uint8_t ILI9XXX_ETMOD     = 0xB7;
 
-static const uint8_t ILI9XXX_PWCTR1 = 0xC0;
-static const uint8_t ILI9XXX_PWCTR2 = 0xC1;
-static const uint8_t ILI9XXX_PWCTR3 = 0xC2;
-static const uint8_t ILI9XXX_PWCTR4 = 0xC3;
-static const uint8_t ILI9XXX_PWCTR5 = 0xC4;
-static const uint8_t ILI9XXX_PWCTR6 = 0xF6;
-static const uint8_t ILI9XXX_VMCTR1 = 0xC5;
-static const uint8_t ILI9XXX_IFCTR = 0xC6;
-static const uint8_t ILI9XXX_VMCTR2 = 0xC7;
-static const uint8_t ILI9XXX_GMCTR = 0xC8;
-static const uint8_t ILI9XXX_SETEXTC = 0xC8;
+static const uint8_t ILI9XXX_PWCTR1    = 0xC0;
+static const uint8_t ILI9XXX_PWCTR2    = 0xC1;
+static const uint8_t ILI9XXX_PWCTR3    = 0xC2;
+static const uint8_t ILI9XXX_PWCTR4    = 0xC3;
+static const uint8_t ILI9XXX_PWCTR5    = 0xC4;
+static const uint8_t ILI9XXX_PWCTR6    = 0xF6;
+static const uint8_t ILI9XXX_VMCTR1    = 0xC5;
+static const uint8_t ILI9XXX_IFCTR     = 0xC6;
+static const uint8_t ILI9XXX_VMCTR2    = 0xC7;
+static const uint8_t ILI9XXX_GMCTR     = 0xC8;
+static const uint8_t ILI9XXX_SETEXTC   = 0xC8;
 
-static const uint8_t ILI9XXX_PWSET = 0xD0;
-static const uint8_t ILI9XXX_VMCTR = 0xD1;
-static const uint8_t ILI9XXX_PWSETN = 0xD2;
-static const uint8_t ILI9XXX_RDID4 = 0xD3;
-static const uint8_t ILI9XXX_RDINDEX = 0xD9;
-static const uint8_t ILI9XXX_RDID1 = 0xDA;
-static const uint8_t ILI9XXX_RDID2 = 0xDB;
-static const uint8_t ILI9XXX_RDID3 = 0xDC;
-static const uint8_t ILI9XXX_RDIDX = 0xDD;  // TBC
+static const uint8_t ILI9XXX_PWSET     = 0xD0;
+static const uint8_t ILI9XXX_VMCTR     = 0xD1;
+static const uint8_t ILI9XXX_PWSETN    = 0xD2;
+static const uint8_t ILI9XXX_RDID4     = 0xD3;
+static const uint8_t ILI9XXX_RDINDEX   = 0xD9;
+static const uint8_t ILI9XXX_RDID1     = 0xDA;
+static const uint8_t ILI9XXX_RDID2     = 0xDB;
+static const uint8_t ILI9XXX_RDID3     = 0xDC;
+static const uint8_t ILI9XXX_RDIDX     = 0xDD;  // TBC
 
-static const uint8_t ILI9XXX_GMCTRP1 = 0xE0;
-static const uint8_t ILI9XXX_GMCTRN1 = 0xE1;
+static const uint8_t ILI9XXX_GMCTRP1   = 0xE0;
+static const uint8_t ILI9XXX_GMCTRN1   = 0xE1;
 
-static const uint8_t ILI9XXX_CSCON = 0xF0;
-static const uint8_t ILI9XXX_ADJCTL3 = 0xF7;
-static const uint8_t ILI9XXX_DELAY_FLAG = 0xFF;
+static const uint8_t ILI9XXX_CSCON     = 0xF0;
+static const uint8_t ILI9XXX_ADJCTL3   = 0xF7;
+static const uint8_t ILI9XXX_DELAY_FLAG= 0xFF;
 
 // Macro for delay in milliseconds
 #define ILI9XXX_DELAY(ms) ((uint8_t) ((ms) | 0x80)), ILI9XXX_DELAY_FLAG
 
 // Parallel interface mode definitions
-static const uint8_t ILI9XXX_CMD_MODE = 0x00;
-static const uint8_t ILI9XXX_DATA_MODE = 0x01;
+static const uint8_t ILI9XXX_CMD_MODE         = 0x00;
+static const uint8_t ILI9XXX_DATA_MODE        = 0x01;
 
 // Commands for parallel mode initialization
-static const uint8_t ILI9XXX_PWCTRB = 0xCF;
-static const uint8_t ILI9XXX_PORCTRB = 0xED;
-static const uint8_t ILI9XXX_DTCA = 0xE8;
-static const uint8_t ILI9XXX_DTCB = 0xEA;
-static const uint8_t ILI9XXX_PWCTRA = 0xCB;
-static const uint8_t ILI9XXX_PUMPRC = 0xF7;
+static const uint8_t ILI9XXX_PWCTRB           = 0xCF;
+static const uint8_t ILI9XXX_PORCTRB          = 0xED;
+static const uint8_t ILI9XXX_DTCA             = 0xE8;
+static const uint8_t ILI9XXX_DTCB             = 0xEA;
+static const uint8_t ILI9XXX_PWCTRA           = 0xCB;
+static const uint8_t ILI9XXX_PUMPRC           = 0xF7;
 
 // Interface mode command
 static const uint8_t ILI9XXX_INTERFACE_CONTROL = 0xF6;
@@ -115,7 +115,7 @@ static const uint8_t ILI9XXX_INTERFACE_CONTROL = 0xF6;
 // Interface mode values
 static const uint8_t ILI9XXX_INTERFACE_8BIT_PARALLEL = 0x00;
 static const uint8_t ILI9XXX_INTERFACE_16BIT_PARALLEL = 0x01;
-static const uint8_t ILI9XXX_INTERFACE_SPI = 0x02;
+static const uint8_t ILI9XXX_INTERFACE_SPI          = 0x02;
 
 // Macros for sending commands and data in parallel mode
 #define ILI9XXX_PARALLEL_CMD(cmd) ((uint8_t) (cmd)), ILI9XXX_CMD_MODE
