520f3ab0885c5c8fd1ff7cee94634c63840adcd9
Al3XKOoL <moonrotation@gmail.com>
2024-08-25 13:13:24 +0100

    Update ili9431_parallel.h

--- esphome/components/ili9341_parallel/ili9431_parallel.h
+++ esphome/components/ili9341_parallel/ili9431_parallel.h
@@ -9,14 +9,14 @@ namespace ili9341_parallel {
 
 class ILI9341ParallelDisplay : public display::DisplayBuffer, public Component {
  public:
-  void set_model_str(const std::string &model) { model_ = model; }
+  void set_model(const std::string &model) { model_ = model; }
+  void set_width(int16_t width) { width_ = width; }
+  void set_height(int16_t height) { height_ = height; }
   void set_dc_pin(GPIOPin *pin) { dc_pin_ = pin; }
   void set_reset_pin(GPIOPin *pin) { reset_pin_ = pin; }
   void set_wr_pin(GPIOPin *pin) { wr_pin_ = pin; }
   void set_rd_pin(GPIOPin *pin) { rd_pin_ = pin; }
   void set_data_pin(int index, GPIOPin *pin) { data_pins_[index] = pin; }
-  void set_width(int16_t width) { width_ = width; }
-  void set_height(int16_t height) { height_ = height; }
 
   void setup() override;
   void dump_config() override;
@@ -25,13 +25,13 @@ class ILI9341ParallelDisplay : public display::DisplayBuffer, public Component {
 
  protected:
   std::string model_;
-  GPIOPin *dc_pin_{nullptr};
-  GPIOPin *reset_pin_{nullptr};
-  GPIOPin *wr_pin_{nullptr};
-  GPIOPin *rd_pin_{nullptr};
-  GPIOPin *data_pins_[8]{nullptr};
-  int16_t width_{240};
-  int16_t height_{320};
+  int16_t width_;
+  int16_t height_;
+  GPIOPin *dc_pin_;
+  GPIOPin *reset_pin_;
+  GPIOPin *wr_pin_;
+  GPIOPin *rd_pin_;
+  GPIOPin *data_pins_[8];
 };
 
 }  // namespace ili9341_parallel
