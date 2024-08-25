2149a548312013aecc802a5c327fa302e1b90b56
Al3XKOoL <moonrotation@gmail.com>
2024-08-25 20:20:10 +0100

    Update tft_espi.h

--- esphome/components/tft_espi/tft_espi.h
+++ esphome/components/tft_espi/tft_espi.h
@@ -6,31 +6,28 @@
 namespace esphome {
 namespace tft_espi {
 
-class TFTeSPIDisplay : public Component, public display::DisplayBuffer {
+class TFTeSPIDisplay : public display::DisplayBuffer {
  public:
-  TFTeSPIDisplay();  // Constructor
-  ~TFTeSPIDisplay(); // Destructor
+  TFTeSPIDisplay();
+  ~TFTeSPIDisplay();
   
   void setup() override;
   void dump_config() override;
   float get_setup_priority() const override;
   void display() override;
-  
-  void set_rotation(uint8_t rotation);
-  void set_invert(bool invert);
+  void set_brightness(float brightness) override;
   void fill(Color color);
   void draw_absolute_pixel_internal(int x, int y, Color color) override;
   int get_width_internal() override;
   int get_height_internal() override;
-  void set_brightness(float brightness) override;
   
  protected:
   Color get_pixel_color(int x, int y);
 
  private:
-  TFT_eSPI *tft_;   // Puntero a TFT_eSPI
-  uint8_t *buffer_; // Buffer de datos de pantalla
-  bool auto_clear_ = true; // Si se debe limpiar la pantalla automáticamente
+  TFT_eSPI *tft_;
+  uint8_t *buffer_;
+  bool auto_clear_ = true;
 };
 
 }  // namespace tft_espi
