22575c12179abd6edb14b7e5afaeaa874e5b2803
Al3XKOoL <moonrotation@gmail.com>
2024-08-25 10:24:19 +0100

    rework2..

--- esphome/components/ili9341_parallel/ili9341_defines.h
+++ esphome/components/ili9341_parallel/ili9341_defines.h
@@ -14,10 +14,7 @@ static const uint8_t MADCTL_BGR   = 0x08;  ///< Bit 3 Blue-Green-Red pixel order
 static const uint8_t MADCTL_MH    = 0x04;   ///< Bit 2 LCD refresh right to left
 // clang-format on
 
-static const uint16_t ILI9341_TFTWIDTH = 320;   ///< ILI9341 max TFT width
-static const uint16_t ILI9341_TFTHEIGHT = 240;  ///< ILI9341 max TFT height
-
-// All ILI9341 specific commands some are used by init()
+// ILI9341 specific commands used by init()
 static const uint8_t ILI9341_NOP = 0x00;
 static const uint8_t ILI9341_SWRESET = 0x01;
 static const uint8_t ILI9341_RDDID = 0x04;
@@ -31,7 +28,7 @@ static const uint8_t ILI9341_NORON = 0x13;
 static const uint8_t ILI9341_RDMODE = 0x0A;
 static const uint8_t ILI9341_RDMADCTL = 0x0B;
 static const uint8_t ILI9341_RDPIXFMT = 0x0C;
-static const uint8_t ILI9341_RDIMGFMT = 0x0A;
+static const uint8_t ILI9341_RDIMGFMT = 0x0D;
 static const uint8_t ILI9341_RDSELFDIAG = 0x0F;
 
 static const uint8_t ILI9341_INVOFF = 0x20;
@@ -46,14 +43,9 @@ static const uint8_t ILI9341_RAMWR = 0x2C;
 static const uint8_t ILI9341_RAMRD = 0x2E;
 
 static const uint8_t ILI9341_PTLAR = 0x30;
-static const uint8_t ILI9341_VSCRDEF = 0x33;
 static const uint8_t ILI9341_MADCTL = 0x36;
 static const uint8_t ILI9341_VSCRSADD = 0x37;
 static const uint8_t ILI9341_PIXFMT = 0x3A;
-
-static const uint8_t ILI9341_WRDISBV = 0x51;
-static const uint8_t ILI9341_RDDISBV = 0x52;
-static const uint8_t ILI9341_WRCTRLD = 0x53;
 
 static const uint8_t ILI9341_FRMCTR1 = 0xB1;
 static const uint8_t ILI9341_FRMCTR2 = 0xB2;
@@ -69,19 +61,19 @@ static const uint8_t ILI9341_PWCTR5 = 0xC4;
 static const uint8_t ILI9341_VMCTR1 = 0xC5;
 static const uint8_t ILI9341_VMCTR2 = 0xC7;
 
-static const uint8_t ILI9341_RDID4 = 0xD3;
-static const uint8_t ILI9341_RDINDEX = 0xD9;
 static const uint8_t ILI9341_RDID1 = 0xDA;
 static const uint8_t ILI9341_RDID2 = 0xDB;
 static const uint8_t ILI9341_RDID3 = 0xDC;
-static const uint8_t ILI9341_RDIDX = 0xDD;  // TBC
+static const uint8_t ILI9341_RDID4 = 0xDD;
 
 static const uint8_t ILI9341_GMCTRP1 = 0xE0;
 static const uint8_t ILI9341_GMCTRN1 = 0xE1;
 
-// Parallel interface specific
-static const uint8_t ILI9341_INTERFACE_CONTROL = 0xF6;
-static const uint8_t ILI9341_INTERFACE_8BIT_PARALLEL = 0x00;
+static const uint8_t ILI9341_PWCTR6 = 0xFC;
+
+static const uint8_t ILI9341_DELAY_FLAG = 0xFF;
+// special marker for delay - command byte represents ms, length byte is an impossible value
+#define ILI9341_DELAY(ms) ((uint8_t) ((ms) | 0x80)), ILI9341_DELAY_FLAG
 
 }  // namespace ili9341_parallel
 }  // namespace esphome
