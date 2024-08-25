c55943d4ff79445ac61731f1947779656bdbc40a
Al3XKOoL <moonrotation@gmail.com>
2024-08-25 12:34:00 +0100

    Update display.py

--- esphome/components/ili9341_parallel/display.py
+++ esphome/components/ili9341_parallel/display.py
@@ -1,53 +1,34 @@
-# esphome/components/ili9341_parallel/display.py
-
 import esphome.codegen as cg
 from esphome import config_validation as cv
+from esphome import pins
 from esphome.components import display
 from esphome.const import CONF_ID
 
-CONF_CS_PIN = "cs_pin"
-CONF_DC_PIN = "dc_pin"
-CONF_RESET_PIN = "reset_pin"
-CONF_D0_PIN = "d0_pin"
-CONF_D1_PIN = "d1_pin"
-CONF_D2_PIN = "d2_pin"
-CONF_D3_PIN = "d3_pin"
-CONF_D4_PIN = "d4_pin"
-CONF_D5_PIN = "d5_pin"
-CONF_D6_PIN = "d6_pin"
-CONF_D7_PIN = "d7_pin"
-
+# Define el namespace para el nuevo controlador
 ili9341_parallel_ns = cg.esphome_ns.namespace('ili9341_parallel')
 ILI9341ParallelDisplay = ili9341_parallel_ns.class_('ILI9341ParallelDisplay', display.DisplayBuffer, cg.Component)
 
+# Configuración del esquema
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(ILI9341ParallelDisplay),
-    cv.Required(CONF_CS_PIN): cv.positive_int,
-    cv.Required(CONF_DC_PIN): cv.positive_int,
-    cv.Required(CONF_RESET_PIN): cv.positive_int,
-    cv.Required(CONF_D0_PIN): cv.positive_int,
-    cv.Required(CONF_D1_PIN): cv.positive_int,
-    cv.Required(CONF_D2_PIN): cv.positive_int,
-    cv.Required(CONF_D3_PIN): cv.positive_int,
-    cv.Required(CONF_D4_PIN): cv.positive_int,
-    cv.Required(CONF_D5_PIN): cv.positive_int,
-    cv.Required(CONF_D6_PIN): cv.positive_int,
-    cv.Required(CONF_D7_PIN): cv.positive_int,
+    cv.Required('cs_pin'): pins.gpio_output_pin_schema,
+    cv.Required('dc_pin'): pins.gpio_output_pin_schema,
+    cv.Required('reset_pin'): pins.gpio_output_pin_schema,
+    cv.Required('wr_pin'): pins.gpio_output_pin_schema,
+    cv.Required('rd_pin'): pins.gpio_output_pin_schema,
+    cv.Required('data_pins'): cv.All([pins.gpio_output_pin_schema], lambda value: value if len(value) == 8 else cv.Invalid("Need 8 data pins")),
 }).extend(cv.COMPONENT_SCHEMA)
 
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    cg.add(var.set_cs_pin(config[CONF_CS_PIN]))
-    cg.add(var.set_dc_pin(config[CONF_DC_PIN]))
-    cg.add(var.set_reset_pin(config[CONF_RESET_PIN]))
-    cg.add(var.set_d0_pin(config[CONF_D0_PIN]))
-    cg.add(var.set_d1_pin(config[CONF_D1_PIN]))
-    cg.add(var.set_d2_pin(config[CONF_D2_PIN]))
-    cg.add(var.set_d3_pin(config[CONF_D3_PIN]))
-    cg.add(var.set_d4_pin(config[CONF_D4_PIN]))
-    cg.add(var.set_d5_pin(config[CONF_D5_PIN]))
-    cg.add(var.set_d6_pin(config[CONF_D6_PIN]))
-    cg.add(var.set_d7_pin(config[CONF_D7_PIN]))
+    cg.add(var.set_cs_pin(config['cs_pin']))
+    cg.add(var.set_dc_pin(config['dc_pin']))
+    cg.add(var.set_reset_pin(config['reset_pin']))
+    cg.add(var.set_wr_pin(config['wr_pin']))
+    cg.add(var.set_rd_pin(config['rd_pin']))
+    
+    pins_ = [yield cg.gpio_pin_expression(pin) for pin in config['data_pins']]
+    cg.add(var.set_data_pins(*pins_))
 
     yield cg.register_component(var, config)
     yield display.register_display(var, config)
