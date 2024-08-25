29b52807d5e185554ce0843d100acb480289f30e
Al3XKOoL <moonrotation@gmail.com>
2024-08-25 14:06:33 +0100

    Update tft_espi_component.cpp

--- esphome/components/tft_espi/tft_espi_component.cpp
+++ esphome/components/tft_espi/tft_espi_component.cpp
@@ -1,9 +1,37 @@
-#include "tft_espi_component.h"
+#include "tft_e_spi_component.h"
+#include "esphome/core/log.h"
 
 namespace esphome {
 namespace tft_espi {
 
-// Puedes agregar implementaciones adicionales aquí si es necesario.
+TFT_eSPIComponent::TFT_eSPIComponent() : tft_(TFT_eSPI()) {}
+
+void TFT_eSPIComponent::setup() {
+  // Aquí configuras el display usando TFT_eSPI
+  tft_.begin();
+  tft_.setRotation(rotation_);
+  tft_.fillScreen(TFT_BLACK);
+}
+
+void TFT_eSPIComponent::loop() {
+  // Puedes agregar actualizaciones periódicas si es necesario
+}
+
+void TFT_eSPIComponent::fill_screen(uint16_t color) {
+  tft_.fillScreen(color);
+}
+
+void TFT_eSPIComponent::print(int x, int y, const char* text, int size, uint16_t color) {
+  tft_.setTextSize(size);
+  tft_.setTextColor(color);
+  tft_.setCursor(x, y);
+  tft_.print(text);
+}
+
+void TFT_eSPIComponent::set_rotation(int rotation) {
+  rotation_ = rotation;
+  tft_.setRotation(rotation_);
+}
 
 }  // namespace tft_espi
 }  // namespace esphome
