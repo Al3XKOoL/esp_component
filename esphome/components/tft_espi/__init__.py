3f6bf9b9e47091255158ad29e1613ae6827b4a77
Al3XKOoL <moonrotation@gmail.com>
2024-08-25 14:29:45 +0100

    Update __init__.py

--- esphome/components/tft_espi/__init__.py
+++ esphome/components/tft_espi/__init__.py
@@ -1,1 +1,17 @@
-# This file can be empty, but it must be present for the component to be recognized.
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import display
+from esphome.const import CONF_ID
+
+DEPENDENCIES = ['esp32']
+
+tft_espi_ns = cg.esphome_ns.namespace('tft_espi')
+TFTeSPIDisplay = tft_espi_ns.class_('TFTeSPIDisplay', display.Display)
+
+CONFIG_SCHEMA = display.BASIC_DISPLAY_SCHEMA.extend({
+    cv.GenerateID(): cv.declare_id(TFTeSPIDisplay),
+}).extend(cv.COMPONENT_SCHEMA)
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await display.register_display(var, config)
