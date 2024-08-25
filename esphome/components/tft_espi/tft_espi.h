f618e1ef9986acf9d827f0da5c9ff7baafcd025c
Al3XKOoL <moonrotation@gmail.com>
2024-08-25 20:17:06 +0100

    Update tft_espi.h

--- esphome/components/tft_espi/tft_espi.h
+++ esphome/components/tft_espi/tft_espi.h
@@ -1,34 +1,36 @@
 #pragma once
 
-#include "esphome/core/component.h"
-#include "esphome/components/display/display_buffer.h"
-#include <TFT_eSPI.h>
+#include "esphome.h"
+#include "TFT_eSPI.h"
 
 namespace esphome {
 namespace tft_espi {
 
-class TFTeSPIDisplay : public display::DisplayBuffer {
+class TFTeSPIDisplay : public Component, public display::DisplayBuffer {
  public:
+  TFTeSPIDisplay();  // Constructor
+  ~TFTeSPIDisplay(); // Destructor
+  
   void setup() override;
-  void display();
-  void update() override;
-  void fill(Color color) override;
+  void dump_config() override;
+  float get_setup_priority() const override;
+  void display() override;
+  
+  void set_rotation(uint8_t rotation);
+  void set_invert(bool invert);
+  void fill(Color color);
   void draw_absolute_pixel_internal(int x, int y, Color color) override;
   int get_width_internal() override;
   int get_height_internal() override;
-  void set_brightness(float brightness);
-  void set_auto_clear(bool auto_clear) { this->auto_clear_ = auto_clear; }
+  void set_brightness(float brightness) override;
   
-  // Cambia esta línea
-  Color get_pixel_color(int x, int y);  // Elimina 'override'
-
-  float get_setup_priority() const override { return setup_priority::PROCESSOR; }
-  display::DisplayType get_display_type() override { return display::DisplayType::DISPLAY_TYPE_COLOR; }
-
  protected:
-    TFT_eSPI *tft_{nullptr};
-    uint8_t *buffer_{nullptr};  // Asegúrate de que esto esté declarado
-    bool auto_clear_{true};  // Agregar soporte para auto_clear_enabled
+  Color get_pixel_color(int x, int y);
+
+ private:
+  TFT_eSPI *tft_;   // Puntero a TFT_eSPI
+  uint8_t *buffer_; // Buffer de datos de pantalla
+  bool auto_clear_ = true; // Si se debe limpiar la pantalla automáticamente
 };
 
 }  // namespace tft_espi
