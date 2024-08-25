e7b86e82abef4cacd7359715d145f79d24b375c6
Al3XKOoL <moonrotation@gmail.com>
2024-08-25 13:49:54 +0100

    Create tft_espi_component.cpp

---
+++ esphome/components/tft_espi/tft_espi_component.cpp
@@ -0,0 +1,9 @@
+#include "tft_espi_component.h"
+
+namespace esphome {
+namespace tft_espi {
+
+// Puedes agregar implementaciones adicionales aquí si es necesario.
+
+}  // namespace tft_espi
+}  // namespace esphome
