31d28e4e508e55fae9cfb737a66b2e352fc1a95f
Al3XKOoL <moonrotation@gmail.com>
2024-08-25 12:36:24 +0100

    Update display.py

--- esphome/components/ili9341_parallel/display.py
+++ esphome/components/ili9341_parallel/display.py
@@ -21,14 +21,25 @@ CONFIG_SCHEMA = cv.Schema({
 
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    cg.add(var.set_cs_pin(config['cs_pin']))
-    cg.add(var.set_dc_pin(config['dc_pin']))
-    cg.add(var.set_reset_pin(config['reset_pin']))
-    cg.add(var.set_wr_pin(config['wr_pin']))
-    cg.add(var.set_rd_pin(config['rd_pin']))
-    
-    pins_ = [cg.gpio_pin_expression(pin) for pin in config['data_pins']]
-    cg.add(var.set_data_pins(*pins_))
 
+    # Asegúrate de que las expresiones son válidas
+    cs_pin = cg.gpio_pin_expression(config['cs_pin'])
+    dc_pin = cg.gpio_pin_expression(config['dc_pin'])
+    reset_pin = cg.gpio_pin_expression(config['reset_pin'])
+    wr_pin = cg.gpio_pin_expression(config['wr_pin'])
+    rd_pin = cg.gpio_pin_expression(config['rd_pin'])
+
+    # Agregar los pines
+    cg.add(var.set_cs_pin(cs_pin))
+    cg.add(var.set_dc_pin(dc_pin))
+    cg.add(var.set_reset_pin(reset_pin))
+    cg.add(var.set_wr_pin(wr_pin))
+    cg.add(var.set_rd_pin(rd_pin))
+
+    # Agregar los pines de datos
+    data_pins = [cg.gpio_pin_expression(pin) for pin in config['data_pins']]
+    cg.add(var.set_data_pins(*data_pins))
+
+    # Registrar el componente y el display
     yield cg.register_component(var, config)
     yield display.register_display(var, config)
