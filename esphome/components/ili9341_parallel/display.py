95fe6ac5be91f44ffb3eaed174f93937b6fafb8c
Al3XKOoL <moonrotation@gmail.com>
2024-08-27 21:00:58 +0100

    Update display.py

--- esphome/components/ili9341_parallel/display.py
+++ esphome/components/ili9341_parallel/display.py
@@ -10,12 +10,12 @@ ILI9341ParallelDisplay = ili9341_parallel_ns.class_("ILI9341ParallelDisplay", di
 
 CONFIG_SCHEMA = display.FULL_DISPLAY_SCHEMA.extend({
     cv.GenerateID(): cv.declare_id(ILI9341ParallelDisplay),
-    cv.Required("data_pins"): cv.All(cv.ensure_list(cv.gpio_output_pin_schema), cv.Length(min=8, max=8)),
-    cv.Required("wr_pin"): cv.gpio_output_pin_schema,
-    cv.Required("rd_pin"): cv.gpio_output_pin_schema,
-    cv.Required("dc_pin"): cv.gpio_output_pin_schema,
-    cv.Optional("reset_pin"): cv.gpio_output_pin_schema,
-    cv.Optional("cs_pin"): cv.gpio_output_pin_schema,
+    cv.Required("data_pins"): cv.All(cv.ensure_list(cv.gpio_pin_schema), cv.Length(min=8, max=8)),
+    cv.Required("wr_pin"): cv.gpio_pin_schema,
+    cv.Required("rd_pin"): cv.gpio_pin_schema,
+    cv.Required("dc_pin"): cv.gpio_pin_schema,
+    cv.Optional("reset_pin"): cv.gpio_pin_schema,
+    cv.Optional("cs_pin"): cv.gpio_pin_schema,
 }).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
