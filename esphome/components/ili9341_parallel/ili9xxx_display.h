fa8f1f4a1cd8b1a4eac703b00f8f7ae5d3acf946
Al3XKOoL <moonrotation@gmail.com>
2024-08-27 21:14:39 +0100

    Update ili9xxx_display.h

--- esphome/components/ili9341_parallel/ili9xxx_display.h
+++ esphome/components/ili9341_parallel/ili9xxx_display.h
@@ -1,6 +1,7 @@
 #pragma once
-#include "esphome/components/display/display.h"
-#include "esphome/components/display/display_color_utils.h"
+#include "esphome/core/component.h"
+#include "esphome/components/display/display_buffer.h"
+#include "esphome/core/gpio.h"  // Añade esta línea
 #include "ili9xxx_defines.h"
 
 #define BUFFER_SIZE 32  // Ajusta este valor según la memoria disponible
@@ -10,7 +11,7 @@ namespace ili9xxx {
 
 static const char *const TAG = "ili9341_parallel";
 
-class ILI9341ParallelDisplay : public display::Display {
+class ILI9341ParallelDisplay : public display::DisplayBuffer {
  public:
   void setup() override;
   void dump_config() override;
@@ -27,9 +28,9 @@ class ILI9341ParallelDisplay : public display::Display {
     this->data_pins_[6] = d6;
     this->data_pins_[7] = d7;
   }
+  void set_dc_pin(GPIOPin *dc_pin) { this->dc_pin_ = dc_pin; }
   void set_wr_pin(GPIOPin *wr_pin) { this->wr_pin_ = wr_pin; }
   void set_rd_pin(GPIOPin *rd_pin) { this->rd_pin_ = rd_pin; }
-  void set_dc_pin(GPIOPin *dc_pin) { this->dc_pin_ = dc_pin; }
   void set_reset_pin(GPIOPin *reset_pin) { this->reset_pin_ = reset_pin; }
   void set_cs_pin(GPIOPin *cs_pin) { this->cs_pin_ = cs_pin; }
 
@@ -45,9 +46,9 @@ class ILI9341ParallelDisplay : public display::Display {
   void write_array(const uint8_t *data, uint16_t length);
 
   GPIOPin *data_pins_[8];
+  GPIOPin *dc_pin_;
   GPIOPin *wr_pin_;
   GPIOPin *rd_pin_;
-  GPIOPin *dc_pin_;
   GPIOPin *reset_pin_{nullptr};
   GPIOPin *cs_pin_{nullptr};
 
