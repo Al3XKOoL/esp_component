301d668908f1092dbb644dcc976c5e518bc2e61e
Al3XKOoL <moonrotation@gmail.com>
2024-08-27 22:27:44 +0100

    Update ili9xxx_display.cpp

--- esphome/components/ili9341_parallel/ili9xxx_display.cpp
+++ esphome/components/ili9341_parallel/ili9xxx_display.cpp
@@ -224,5 +224,13 @@ void ILI9341ParallelDisplay::set_data_pins(GPIOPin *d0, GPIOPin *d1, GPIOPin *d2
   this->data_pins_[7] = d7;
 }
 
+void ILI9341ParallelDisplay::set_data_pin(uint8_t index, GPIOPin *pin) {
+  if (index < 8) {
+    this->data_pins_[index] = pin;
+  } else {
+    ESP_LOGE(TAG, "Invalid data pin index: %d", index);
+  }
+}
+
 }  // namespace ili9xxx
 }  // namespace esphome
