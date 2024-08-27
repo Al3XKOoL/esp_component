c9115c2644b604666416bc08616e1eebdaebbddb
Al3XKOoL <moonrotation@gmail.com>
2024-08-27 21:43:00 +0100

    Update ili9xxx_display.h

--- esphome/components/ili9341_parallel/ili9xxx_display.h
+++ esphome/components/ili9341_parallel/ili9xxx_display.h
@@ -14,24 +14,18 @@ class ILI9341ParallelDisplay : public display::DisplayBuffer {
   void setup() override;
   void dump_config() override;
   float get_setup_priority() const override;
-  void display();
+  void display() override;
   void update() override;
-  void set_data_pins(GPIOPin *d0, GPIOPin *d1, GPIOPin *d2, GPIOPin *d3, GPIOPin *d4, GPIOPin *d5, GPIOPin *d6, GPIOPin *d7) {
-    this->data_pins_[0] = d0;
-    this->data_pins_[1] = d1;
-    this->data_pins_[2] = d2;
-    this->data_pins_[3] = d3;
-    this->data_pins_[4] = d4;
-    this->data_pins_[5] = d5;
-    this->data_pins_[6] = d6;
-    this->data_pins_[7] = d7;
-  }
-  void set_dc_pin(GPIOPin *dc_pin) { this->dc_pin_ = dc_pin; }
-  void set_wr_pin(GPIOPin *wr_pin) { this->wr_pin_ = wr_pin; }
-  void set_rd_pin(GPIOPin *rd_pin) { this->rd_pin_ = rd_pin; }
-  void set_reset_pin(GPIOPin *reset_pin) { this->reset_pin_ = reset_pin; }
-  void set_cs_pin(GPIOPin *cs_pin) { this->cs_pin_ = cs_pin; }
+  
+  // Implementa estos métodos
+  display::DisplayType get_display_type() override { return display::DisplayType::DISPLAY_TYPE_COLOR; }
+  int get_width_internal() override { return this->width_; }
+  int get_height_internal() override { return this->height_; }
+  
+  void set_width(int width) { this->width_ = width; }
+  void set_height(int height) { this->height_ = height; }
   void set_rotation(display::DisplayRotation rotation);
+  void set_data_pins(GPIOPin *d0, GPIOPin *d1, GPIOPin *d2, GPIOPin *d3, GPIOPin *d4, GPIOPin *d5, GPIOPin *d6, GPIOPin *d7);
 
  protected:
   void draw_absolute_pixel_internal(int x, int y, Color color) override;
@@ -41,20 +35,15 @@ class ILI9341ParallelDisplay : public display::DisplayBuffer {
   void send_command_(uint8_t cmd);
   void send_data_(uint8_t data);
   void write_byte(uint8_t value);
-  void write_array(const uint8_t *data, uint16_t length);
-  uint8_t read_byte();
-  uint32_t read_command(uint8_t command, uint8_t index);
-  void read_display_identification_information();
 
+  int width_{240};
+  int height_{320};
   GPIOPin *data_pins_[8];
   GPIOPin *dc_pin_;
   GPIOPin *wr_pin_;
   GPIOPin *rd_pin_;
   GPIOPin *reset_pin_{nullptr};
   GPIOPin *cs_pin_{nullptr};
-
-  int16_t width_{240};
-  int16_t height_{320};
   display::DisplayRotation rotation_{display::DISPLAY_ROTATION_0_DEGREES};
 };
 
