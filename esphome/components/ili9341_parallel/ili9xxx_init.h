25b881fdf52023724356ce2ab59c98ac99413526
Al3XKOoL <moonrotation@gmail.com>
2024-08-28 20:49:18 +0100

    Update ili9xxx_init.h

--- esphome/components/ili9341_parallel/ili9xxx_init.h
+++ esphome/components/ili9341_parallel/ili9xxx_init.h
@@ -1,52 +1,5 @@
 #pragma once
 
-// ILI9341 LCD Registers
-#define ILI9XXX_NOP         0x00
-#define ILI9XXX_SWRESET     0x01
-#define ILI9XXX_RDDID       0x04
-#define ILI9XXX_RDDST       0x09
-#define ILI9XXX_SLPIN       0x10
-#define ILI9XXX_SLPOUT      0x11
-#define ILI9XXX_PTLON       0x12
-#define ILI9XXX_NORON       0x13
-#define ILI9XXX_RDMODE      0x0A
-#define ILI9XXX_RDMADCTL    0x0B
-#define ILI9XXX_RDPIXFMT    0x0C
-#define ILI9XXX_RDIMGFMT    0x0D
-#define ILI9XXX_RDSELFDIAG  0x0F
-#define ILI9XXX_INVOFF      0x20
-#define ILI9XXX_INVON       0x21
-#define ILI9XXX_GAMMASET    0x26
-#define ILI9XXX_DISPOFF     0x28
-#define ILI9XXX_DISPON      0x29
-#define ILI9XXX_CASET       0x2A
-#define ILI9XXX_PASET       0x2B
-#define ILI9XXX_RAMWR       0x2C
-#define ILI9XXX_RAMRD       0x2E
-#define ILI9XXX_PTLAR       0x30
-#define ILI9XXX_MADCTL      0x36
-#define ILI9XXX_VSCRSADD    0x37
-#define ILI9XXX_PIXFMT      0x3A
-#define ILI9XXX_FRMCTR1     0xB1
-#define ILI9XXX_FRMCTR2     0xB2
-#define ILI9XXX_FRMCTR3     0xB3
-#define ILI9XXX_INVCTR      0xB4
-#define ILI9XXX_DFUNCTR     0xB6
-#define ILI9XXX_PWCTR1      0xC0
-#define ILI9XXX_PWCTR2      0xC1
-#define ILI9XXX_PWCTR3      0xC2
-#define ILI9XXX_PWCTR4      0xC3
-#define ILI9XXX_PWCTR5      0xC4
-#define ILI9XXX_VMCTR1      0xC5
-#define ILI9XXX_VMCTR2      0xC7
-#define ILI9XXX_RDID1       0xDA
-#define ILI9XXX_RDID2       0xDB
-#define ILI9XXX_RDID3       0xDC
-#define ILI9XXX_RDID4       0xDD
-#define ILI9XXX_GMCTRP1     0xE0
-#define ILI9XXX_GMCTRN1     0xE1
-#define ILI9XXX_COLMOD      0x3A
-
 #include "esphome/core/hal.h"
 #include "ili9xxx_defines.h"
 #include <pgmspace.h>
