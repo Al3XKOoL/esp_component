42fe8864d16a31b03bca2f1a4472b4b555dcc529
Al3XKOoL <moonrotation@gmail.com>
2024-08-28 19:33:55 +0100

    Update ili9xxx_display.h

--- esphome/components/ili9341_parallel/ili9xxx_display.h
+++ esphome/components/ili9341_parallel/ili9xxx_display.h
@@ -11,38 +11,26 @@ class ILI9341ParallelDisplay : public display::DisplayBuffer {
  public:
   void setup() override;
   void dump_config() override;
-  float get_setup_priority() const override;
   void update() override;
   void fill(Color color) override;
   display::DisplayType get_display_type() override { return display::DisplayType::DISPLAY_TYPE_COLOR; }
 
+  void set_dc_pin(GPIOPin *dc_pin) { dc_pin_ = dc_pin; }
+  void set_reset_pin(GPIOPin *reset_pin) { reset_pin_ = reset_pin; }
+  void set_wr_pin(GPIOPin *wr_pin) { wr_pin_ = wr_pin; }
+  void set_rd_pin(GPIOPin *rd_pin) { rd_pin_ = rd_pin; }
+  void set_cs_pin(GPIOPin *cs_pin) { cs_pin_ = cs_pin; }
   void set_data_pins(GPIOPin *d0, GPIOPin *d1, GPIOPin *d2, GPIOPin *d3,
                      GPIOPin *d4, GPIOPin *d5, GPIOPin *d6, GPIOPin *d7);
-  void set_data_pin(uint8_t index, GPIOPin *pin);
-  void set_dc_pin(GPIOPin *dc_pin) { this->dc_pin_ = dc_pin; }
-  void set_reset_pin(GPIOPin *reset_pin) { this->reset_pin_ = reset_pin; }
-  void set_cs_pin(GPIOPin *cs_pin) { this->cs_pin_ = cs_pin; }
-  void set_wr_pin(GPIOPin *wr_pin) { this->wr_pin_ = wr_pin; }
-  void set_rd_pin(GPIOPin *rd_pin) { this->rd_pin_ = rd_pin; }
-
-  void set_width(uint16_t width) { this->width_ = width; }
-  void set_height(uint16_t height) { this->height_ = height; }
-  void set_rotation(int rotation);
 
  protected:
-  static uint16_t color_to_rgb565(Color color);
   void draw_absolute_pixel_internal(int x, int y, Color color) override;
-  int get_width_internal() override;
-  int get_height_internal() override;
+  int get_width_internal() override { return width_; }
+  int get_height_internal() override { return height_; }
 
-  void write_display_data_();
   void init_lcd_();
-  void reset_();
   void send_command_(uint8_t cmd);
   void send_data_(uint8_t data);
-  void write_byte(uint8_t value);
-
-  Color get_pixel_color_(int x, int y) const;
 
   GPIOPin *data_pins_[8]{};
   GPIOPin *dc_pin_{nullptr};
@@ -52,7 +40,6 @@ class ILI9341ParallelDisplay : public display::DisplayBuffer {
   GPIOPin *cs_pin_{nullptr};
   uint16_t width_{320};
   uint16_t height_{240};
-  int rotation_{0};
 };
 
 }  // namespace ili9xxx
