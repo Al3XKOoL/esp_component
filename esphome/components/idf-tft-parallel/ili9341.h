0c7e2101ddf1ddc11677c3d020996d7fdc817398
Al3XKOoL <moonrotation@gmail.com>
2024-08-31 08:24:01 +0100

    Update ili9341.h

--- esphome/components/idf-tft-parallel/ili9341.h
+++ esphome/components/idf-tft-parallel/ili9341.h
@@ -16,6 +16,7 @@ private:
     int width_;
     int height_;
     INTERFACE _interface; // Cambiar a tipo INTERFACE
+    GPIOPin *_d0, *_d1, *_d2, *_d3, *_d4, *_d5, *_d6, *_d7; // Pines de datos
 };
 
 #endif
