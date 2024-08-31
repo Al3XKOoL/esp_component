243a17f96191a900c705e947155747f034ee5346
Al3XKOoL <moonrotation@gmail.com>
2024-08-31 07:55:58 +0100

    Create ili9341.h

---
+++ esphome/components/idf-tft-parallel/ili9341.h
@@ -0,0 +1,21 @@
+#ifndef __ILI9341_H__
+#define __ILI9341_H__
+
+#include "lcd_lib.h"
+
+class ILI9341Component : public Component {
+public:
+    void setup() override;
+    void loop() override;
+
+    void set_width(int width);
+    void set_height(int height);
+    void set_interface(int interface);
+
+private:
+    int width_;
+    int height_;
+    int interface_;
+};
+
+#endif
